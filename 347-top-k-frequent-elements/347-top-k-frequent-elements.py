6a6aff12ba9f8ac16856b5a3a48e27bbd96176c0
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-02-22 14:05:48 +0300

    Time: 219 ms (7.98%), Space: 18.8 MB (53.73%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -17,22 +17,3 @@ class Solution:
             frequency[j] = 0
             ans.append(numbers[j])
         return ans
-        
-        # unique = set(nums)
-        # dic = {}
-        # for i in unique:
-        #     dic[i] = nums.count(i)
-        # nwv = list(dic.values())
-        # nwk = list(dic.keys())
-        # check = nwv[:]
-        # check.sort()
-        # print(nwk)
-        # print(nwv)
-        # print(check)
-        # ld = []
-        # for i in range(k):
-        #     m = nwv.index(check[-1-i])
-        #     nwv[m] = 0
-        #     print(m)
-        #     ld.append(nwk[m])
-        # return ld  
