a80cdfb3edde37c0cc4bcd3038f66fd6de08a9bf
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-04-15 12:28:44 +0300

    Time: 179 ms (59.52%), Space: 17.9 MB (10.12%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,28 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        def dfs(i,j,r,c,ans):
+        isValid = lambda r,c: 0<=r<len(grid) and 0<=c<len(grid[0])
+        nxt = [0,1,0,-1,0]
+        vstd = set()
+        def dfs(r,c):
             self.ans += 1
-            self.not_include.append([i,j])
-            if j+1<c and grid[i][j+1] == 1 and [i,j+1] not in self.not_include:
-                dfs(i,j+1,r,c,ans)
-            if j-1>=0 and grid[i][j-1] == 1 and [i,j-1] not in self.not_include:
-                dfs(i,j-1,r,c,ans)
-            if i+1<r and grid[i+1][j] == 1 and [i+1,j] not in self.not_include:
-                dfs(i+1,j,r,c,ans)
-            if i-1>=0 and grid[i-1][j] == 1 and [i-1,j] not in self.not_include:
-                dfs(i-1,j,r,c,ans)
-            return ans
+            vstd.add((r,c))
+            for j in range(len(nxt)-1):
+                if isValid(r+nxt[j],c+nxt[1+j]) and (r+nxt[j],c+nxt[1+j]) not in vstd and grid[r+nxt[j]][c+nxt[1+j]] == 1:
+                    dfs(r+nxt[j],c+nxt[1+j])
         answer = 0
-        self.not_include = []
-        row = len(grid)
-        column = len(grid[0])
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][j] == 1 and [i,j] not in self.not_include:
+                if (i,j) not in vstd and grid[i][j] == 1:
                     self.ans = 0
-                    area = dfs(i,j,row,column,0)
+                    dfs(i,j)
                     answer = max(answer,self.ans)
         return answer
-                    
-        
-            
