efe47721aad16a19d6398990a959707c404c18fe
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-04-29 13:20:35 +0300

    Time: 84 ms (39.86%), Space: 14.2 MB (5.71%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,20 +1,52 @@
-import heapq
+class Node:
+    def __init__(self):
+        self.letters = {}
+        self.isEnd = False
+        self.count = 0
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for i in word:
+            if i not in current.letters:
+                current.letters[i] = Node()
+            current = current.letters[i]
+        current.isEnd = True
+        current.count += 1
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for i in word:
+            if i not in current.letters:
+                return False
+            current = current.letters[i]
+        return current.count
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for i in prefix:
+            if i not in current.letters:
+                return False
+            current = current.letters[i]
+        return True
+
 class Solution:
-    def topKFrequent(self, nums: List[str], k: int) -> List[str]:
-        dic = {}
-        nums.sort()
-        for i in nums:
-            if i in dic:
-                dic[i] += 1
-            else:
-                dic[i] = 1
-        frequency = list(dic.values())
-        numbers = list(dic.keys())
-        heaplist = [-i for i in frequency]
-        heapq.heapify(heaplist)
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        word = Trie()
         ans = []
-        for i in range(k):
-            j = frequency.index(-1*heapq.heappop(heaplist))
-            frequency[j] = 0
-            ans.append(numbers[j])
+        count = defaultdict(set)
+        for i in words: word.insert(i)
+        for i in words: count[word.search(i)].add(i)
+        keys = list(count.keys())
+        keys.sort(reverse = True)
+        for i in keys:      
+            for j in sorted(count[i]):
+                ans.append(j)
+                if len(ans) == k:
+                    return ans
         return ans
+        
+        
