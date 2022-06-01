4e05db0c763f94813b63be50ca133cc40349b002
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-06-01 13:10:55 +0300

    Time: 46 ms (39.45%), Space: 14 MB (29.70%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -3,22 +3,21 @@ class Solution:
         dic = {'2': ['a','b','c'],'3': ['d','e','f'],'4': ['g','h','i'],
             '5': ['j','k','l'],'6': ['m','n','o'],'7': ['p','q','r','s'],
             '8': ['t','u','v'],'9': ['w','x','y','z']}
-        hold = {}
-        def backtrack(idx):
-            if idx in hold:
-                return hold[idx]
+        ans = []
+        dp = {}
+        def dfs(idx):
+            if idx in dp:
+                return dp[idx]
             if idx == len(digits)-1:
-                hold[idx] = dic[digits[idx]]
+                dp[idx] = dic[digits[idx]]
                 return dic[digits[idx]]
-            val = []
+            temp = []
             for i in dic[digits[idx]]:
-                temp = backtrack(idx+1)
-                for j in temp:
-                    val.append(i+j)
-            hold[idx] = val
-            return val
-        if len(digits) == 0:
-            return []
-        return backtrack(0)
-            
+                added = dfs(idx+1)
+                for j in added:
+                    temp.append(i+j)
+            dp[idx] = temp
+            return temp
+        if digits:
+            return dfs(0)
             
