d4e9620dec7c4110284970f4669257d3808ac011
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-07-31 16:38:49 +0300

    Time: 56 ms (33.23%), Space: 13.9 MB (66.03%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,12 @@
 class Solution:
-    def gain(self,i,dic,nums):
-        if i>=len(nums):
-            return 0
-        if i in dic:
-            return dic[i]
-        dic[i] = max(self.gain(i+2,dic,nums), self.gain(i+3,dic,nums)) +nums[i]
-        return dic[i]
     def rob(self, nums: List[int]) -> int:
-        dic = {}
-        return max(self.gain(0,dic,nums),self.gain(1,dic,nums))
+        memo = {}
+        def rec(i):
+            if i >= len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i] = nums[i] + max(rec(i+3),rec(i+2))
+            return memo[i]
+        return max(rec(0),rec(1))
+        
