fdd78f9bfc455ae0d667bb6c9d95f90f0d1d749c
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-02 20:24:25 +0300

    Time: 177 ms (38.25%), Space: 15.9 MB (30.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,12 +1,14 @@
 class Solution:
-    def minn(self,grid,r,c,dic):
-        if r == len(grid)-1 and c == len(grid[0])-1:
-            return grid[r][c]
-        if r>=len(grid) or c>=len(grid[0]):
-            return 1000000
-        if (r,c) in dic: return dic[(r,c)]
-        dic[r,c] = grid[r][c] + min(self.minn(grid,r,c+1,dic),self.minn(grid,r+1,c,dic))
-        return dic[(r,c)]
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dic = {}
-        return self.minn(grid,0,0,dic)
+        ans = [[float('inf') for _ in range(len(grid[0]))] for j in range(len(grid))]
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if i == 0 and j == 0:
+                    ans[i][j] = grid[i][j]
+                elif i == 0:
+                    ans[i][j] = ans[i][j-1]+grid[i][j]
+                elif j == 0:
+                    ans[i][j] = ans[i-1][j]+grid[i][j]
+                else: 
+                    ans[i][j] = min(ans[i-1][j],ans[i][j-1])+grid[i][j]
+        return ans[-1][-1]
