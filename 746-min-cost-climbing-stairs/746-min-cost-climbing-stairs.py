2868788a1f44a8b125abca405ef3de766fdc3a1c
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-03 17:01:45 +0300

    Time: 111 ms (26.13%), Space: 16.3 MB (6.90%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,12 +1,11 @@
 class Solution:
-    def costs(self,cost,i,dic):
-        if i>=len(cost):
-            return 0
-        if i in dic: 
-            return dic[i]
-        else:
-            dic[i] = cost[i] + min(self.costs(cost,i+1,dic),self.costs(cost,i+2,dic))
-            return dic[i]
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dic = {}
-        return min(self.costs(cost,0,dic),self.costs(cost,1,dic))
+        memo = {}
+        def dp(idx):
+            if idx>len(cost)-1:
+                return 0
+            if idx in memo:
+                return memo[idx]
+            memo[idx] = min(dp(idx+1),dp(idx+2))+cost[idx]
+            return memo[idx]
+        return min(dp(0),dp(1))
