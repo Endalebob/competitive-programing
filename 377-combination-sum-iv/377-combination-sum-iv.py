cdc00b85a1ee2f0c1345361ca0748bd4e6f7901d
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-05 10:36:35 +0300

    Time: 392 ms (5.17%), Space: 25.4 MB (5.17%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,10 +1,7 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        ans = [0]
-        memo = {}
+        @cache
         def rec(s,g):
-            if (s,g) in memo:
-                return memo[(s,g)]
             if s > target:
                 return 0
             if s == target:
@@ -12,7 +9,5 @@ class Solution:
             temp = 0
             for i in nums:
                 temp += rec(s+i,i)
-            memo[(s,g)] = temp
             return temp
-        # rec(0,0)
         return rec(0,0)
