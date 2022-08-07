94136a7607dc701f0f190913fd5863f31b44564c
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-07 16:03:14 +0300

    Time: 120 ms (48.91%), Space: 14.3 MB (14.38%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,23 +1,22 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        self.image = image
-        wanted = image[sr][sc]
-        column_length = len(image[sr])
-        row_length = len(image)
-        hold = []
-        return self.dfs(image,sr,sc,newColor,wanted,row_length,column_length,hold)
-    def dfs(self,image,i,j,new,old,r,l,hold):
-        image[i][j] = new
-        hold.append([i,j])
-        if j+1<l and image[i][j+1] == old and [i,j+1] not in hold:
-            self.dfs(image,i,j+1,new,old,r,l,hold)
-        if j-1>=0 and image[i][j-1] == old and [i,j-1] not in hold:
-            self.dfs(image,i,j-1,new,old,r,l,hold)
-        if i+1<r and image[i+1][j] == old and [i+1,j] not in hold:
-            self.dfs(image,i+1,j,new,old,r,l,hold)
-        if i-1>=0 and image[i-1][j] == old and [i-1,j] not in hold:
-            self.dfs(image,i-1,j,new,old,r,l,hold)
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        begin = image[sr][sc]
+        if begin == color:
+            return image
+        row,col = len(image),len(image[0])
+        is_valid = lambda r,c: 0<=r<row and 0<=c<col
+        dirc = [0,1,0,-1,0]
+        vstd = set()
+        def rec(r,c):
+            if (r,c) in vstd:
+                return
+            vstd.add((r,c))
+            for i in range(4):
+                nr,nc = r+dirc[i],c+dirc[i+1]
+                if is_valid(nr,nc) and image[nr][nc] == image[sr][sc]:
+                    rec(nr,nc)
+            image[r][c] = color
+        rec(sr,sc)
         return image
-        
-
+            
         
