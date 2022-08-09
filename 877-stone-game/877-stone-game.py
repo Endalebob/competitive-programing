f3c0dfbe29dcc6fccc4bb2c9a6f5c1678aebe74d
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-09 04:55:19 +0300

    Time: 176 ms (49.52%), Space: 25.7 MB (37.23%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,26 +1,26 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        @cache
+        memo = {}
         def dp(turn,l,r,alice,bob):
             if l > r:
                 return alice > bob
-            # if (turn,l,r) in memo:
-            #     return memo[(turn,l,r)]
+            if (turn,l,r) in memo:
+                return memo[(turn,l,r)]
             if turn == 0:
                 if dp(1-turn,l+1,r,alice+piles[l],bob):
-                    # memo[(turn,l,r)] = True
+                    memo[(turn,l,r)] = True
                     return True
                 if dp(1-turn,l,r-1,alice+piles[r],bob):
-                    # memo[turn,l,r] = True
+                    memo[turn,l,r] = True
                     return True
             else:
                 if dp(1-turn,l+1,r,alice,piles[l]+bob):
-                    # memo[turn,l,r] = True
+                    memo[turn,l,r] = True
                     return True
                 if dp(1-turn,l,r-1,alice,piles[r]+bob):
-                    # memo[turn,l,r] = True
+                    memo[turn,l,r] = True
                     return True
-            # memo[(turn,l,r)] = False
+            memo[(turn,l,r)] = False
             return False
         return dp(0,0,len(piles)-1,0,0)
             
