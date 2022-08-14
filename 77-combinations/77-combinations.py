553338f03ed0674929968e3a3c165474d07dedb6
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-14 18:14:55 +0300

    Time: 101 ms (95.22%), Space: 14.8 MB (99.20%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,13 +1,3 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-        def comb(l,temp):
-            if len(temp) == k:
-                result.append(temp.copy())
-                return
-            for i in range(l,n+1):
-                temp.append(i)
-                comb(i + 1,temp)
-                temp.pop()
-        comb(1,[])
-        return result
+        return combinations(range(1,n+1),k)
