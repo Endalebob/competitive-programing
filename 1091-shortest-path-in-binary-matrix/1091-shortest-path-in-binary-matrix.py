234e0a5d2f449d89131a91ff6362a7437a9d5510
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-23 10:33:09 +0300

    Time: 1245 ms (24.11%), Space: 15.4 MB (25.86%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,18 +1,26 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        possible_path = ((0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1))
-        is_valid = lambda r,c : 0 <= r < len(grid) and 0<= c < len(grid[0])
-        deq = deque()
+        row,col = len(grid),len(grid[0])
+        isValid = lambda r,c : 0<=r<row and 0<=c<col
         if grid[0][0] == 0:
-            deq.append((0,0,1))
-            grid[0][0] = 1
-        while deq:
-            row,col,length = deq.popleft()
-            if row == len(grid)-1 and col == len(grid[0])-1:
-                return length
-            for i in possible_path:
-                if is_valid(row+i[0],col+i[1]) and grid[row+i[0]][col+i[1]] == 0:
-                    deq.append((row+i[0],col+i[1],length+1))
-                    grid[row+i[0]][col+i[1]] = 1
+            heap = [[1,(0,0)]]
+        else:
+            heap = []
+        heapq.heapify(heap)
+        vstd = set()
+        dirc = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+        while heap:
+            curr = heapq.heappop(heap)
+            if curr[1] in vstd:
+                continue
+            else:
+                vstd.add(curr[1])
+            if curr[1] == (row-1,col-1):
+                return curr[0]
+            for i in dirc:
+                r,c = curr[1][0]+i[0],curr[1][1]+i[1]
+                if isValid(r,c) and grid[r][c] == 0:
+                    if (r,c) not in vstd:
+                        heapq.heappush(heap,[curr[0]+1,(r,c)])
         return -1
-                    
+            
