20113cf09f510530b802d2eb1b77115757f9baaf
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-24 20:16:13 +0300

    Time: 3710 ms (45.41%), Space: 119.8 MB (42.56%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,48 +1,35 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        if len(points)<2:
-            return 0
-        graph = self.mk_graph(points)
-        graph.sort(key = lambda x:x[2])
-        Set = UnionFind(len(points))
-        count = 1
-        result = 0
-
-        for i in graph:
-            e1,e2,cost = i
-            if count == len(points):
-                return result
-            if Set.union(e1,e2):
-                result += cost
-                count += 1
-        return result
-
-    def mk_graph(self,pts):
-        graph = []
-        for i in range(len(pts)-1):
-            for j in range(i+1,len(pts)):
-                val = abs(pts[i][0]-pts[j][0]) + abs(pts[i][1]-pts[j][1])
-                graph.append([i,j,val])
-        return graph
+        heap = []
+        for i in range(len(points)):
+            for j in range(i+1,len(points)):
+                dis = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])
+                heapq.heappush(heap,[dis,(i,j)])
+        ans = 0
+        UF = UnionFind(len(points))
+        while heap:
+            curr = heapq.heappop(heap)
+            a = UF.find(curr[1][0])
+            if a == UF.find(curr[1][1]):
+                if UF.rank[a] == len(points):
+                    break
+                continue
+            ans += curr[0]
+            UF.union(curr[1][0],curr[1][1])
+        return ans
+    
 class UnionFind:
     def __init__(self,n):
         self.root = [i for i in range(n)]
-        self.rank = [1 for i in self.root]
-    def find(self, vertex):
-        if vertex == self.root[vertex]:
-            return vertex
-        self.root[vertex] = self.find(self.root[vertex])
-        return self.root[vertex]
-    def union(self,e1,e2):
-        node1 = self.find(e1)
-        node2 = self.find(e2)
-        if node1 != node2:
-            if self.rank[node2]>self.rank[node1]:
-                self.root[node1] = node2
-            elif self.rank[node2]<self.rank[node1]:
-                self.root[node2] = node1
-            else:
-                self.root[node2] = node1
-                self.rank[node1] += 1
-            return True
-        return False
+        self.rank = [1 for i in range(n)]
+    def find(self, x):
+        while x != self.root[x]:
+            x = self.root[x]
+        return x
+    def union(self,aa,bb):
+        a = self.find(aa)
+        b = self.find(bb)
+        if self.rank[a]<self.rank[b]:
+            a,b = b,a
+        self.rank[a] += self.rank[b]
+        self.root[b] = a
