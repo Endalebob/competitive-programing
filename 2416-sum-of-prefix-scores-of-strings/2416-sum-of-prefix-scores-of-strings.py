08ec73c70a2705c6999fc5d5097e2583a4e69e7d
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-09-22 20:03:47 +0300

    Time: 2586 ms (94.49%), Space: 166.1 MB (63.38%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self):
+        self.letters = {}
+        self.count = 0
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for i in word:
+            if i not in current.letters:
+                current.letters[i] = Node()
+            # current.count += 1
+            current = current.letters[i]
+            current.count += 1
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        ans = 0
+        for i in word:
+            current = current.letters[i]
+            ans += current.count
+        return ans
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        dic = Trie()
+        for i in words:
+            dic.insert(i)
+        ans = []
+        for i in words:
+            ans.append(dic.search(i))
+        return ans
