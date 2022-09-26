5cfb1c3c575842485cad141e5f989923085936d9
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-09-26 22:35:20 +0300

    Time: 3758 ms (14.85%), Space: 14.2 MB (62.38%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
@@ -1,4 +1,4 @@
-class DSU:
+class UnionFind:
     def __init__(self,nodes):
         self.root = [i for i in range(nodes)]
         self.rank = [1 for i in range(nodes)]
@@ -8,30 +8,42 @@ class DSU:
         return node
     def union(self,node1,node2):
         a,b = self.find(node1),self.find(node2)
-        if a == b:
-            return False
         if self.rank[a]<self.rank[b]:
             a,b = b,a
         self.rank[a] += 1
         self.root[b] = a
-        return True
 class Solution:
     def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        edges = sorted((w, u, v, i) for i, (u, v, w) in enumerate(edges))
+        
         critical, pseudo = [], []
-        for _w, _u, _v, i in edges:
-            dsu1, dsu2 = DSU(n), DSU(n)
-            dsu1.union(_u, _v)
-            s1, s2 = _w, 0
-            for w, u, v, j in edges:
-                if i == j:
-                    continue
-                if dsu1.union(u, v):
-                    s1 += w
-                if dsu2.union(u, v):
-                    s2 += w
-            if s1 == s2:
-                pseudo.append(i)
-            elif s1 < s2 or dsu2.union(_u, _v):
-                critical.append(i)
-        return critical, pseudo
+        dic = {}
+        for i, v in enumerate(edges):
+            dic[tuple(v)] = i
+        
+        def kruskal():
+            edge = sorted([w, u, v, i] for i, (u, v, w) in enumerate(edges))
+            uf = UnionFind(n)
+            cost = 0
+            for i in edge:
+                a,b = uf.find(i[1]),uf.find(i[2])
+                if a != b:
+                    uf.union(a,b)
+                    cost += i[0]
+            return cost
+        mnv = kruskal()
+        for i in range(len(edges)):
+            temp = edges[i][2]
+            edges[i][2] += 1
+            d = kruskal()
+            if mnv < d:
+                edges[i][2] -= 1
+                critical.append(dic[tuple(edges[i])])
+            elif d == mnv:
+                edges[i][2] -= 2
+                if mnv > kruskal():
+                    edges[i][2] += 1
+                    pseudo.append(dic[tuple(edges[i])])
+            edges[i][2] = temp
+        return critical,pseudo
+                
+            
