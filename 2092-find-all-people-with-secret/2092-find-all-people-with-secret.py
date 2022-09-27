0ca6c6a094917c4e9ef0834e5aad0f05b9d0a975
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-09-27 12:47:56 +0300

    Time: 6636 ms (7.80%), Space: 145.4 MB (5.30%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,30 +1,24 @@
-from collections import defaultdict
-
 class Solution:
-    def getParent(self, parent, x):
-        if x != parent[x]:
-            parent[x] = self.getParent(parent,parent[x])
-        return parent[x]
-    
-    def connect(self, parent, a, b):
-        parent[self.getParent(parent, a)] = self.getParent(parent, b)
-        
-    def isConnected(self, parent, a, b):
-        return self.getParent(parent, a) == self.getParent(parent, b)
-    
-    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        parent = list(range(n))
-        meets = defaultdict(list)
-        for a, b, t in meetings:
-            meets[t].append((a, b))
-        self.connect(parent, 0, firstPerson)
-        # people = set()
-        for t in sorted(meets.keys()):
-            people = set()
-            for a, b in meets[t]:
-                self.connect(parent, a, b)
-                people.update({a,b})
-            for p in people:
-                if not self.isConnected(parent, p, 0):
-                    parent[p] = p
-        return [i for i in range(n) if self.isConnected(parent, i, 0)]
+    def find(self,a):
+        if a != self.root[a]:
+            self.root[a] = self.find(self.root[a])
+        return self.root[a]
+    def union(self,a,b):
+        self.root[self.find(b)] = self.find(a)
+    def isConnected(self,a,b):
+        return self.find(a) == self.find(b)
+    def findAllPeople(self, n: int, m: List[List[int]], f: int) -> List[int]:
+        self.root = [i for i in range(n)]
+        self.union(0,f)
+        dic = defaultdict(list)
+        for i in m:
+            dic[i[2]].append(i[:2])
+        for i in sorted(dic.keys()):
+            vstd = set()
+            for a,b in dic[i]:
+                self.union(a,b)
+                vstd.update({a,b})
+            for j in vstd:
+                if not self.isConnected(0,j):
+                    self.root[j] = j
+        return [i for i in range(n) if self.isConnected(0,i)]  
