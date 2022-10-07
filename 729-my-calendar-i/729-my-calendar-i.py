40e0859d32dba250596bca8ea5b78cf430798614
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-07 11:35:07 +0300

    Time: 5738 ms (5.02%), Space: 36.6 MB (5.92%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,13 +1,31 @@
 class MyCalendar:
 
     def __init__(self):
-        self.calander = []
+        self.seg = defaultdict(int)
+        self.lazy = defaultdict(int)
 
     def book(self, start: int, end: int) -> bool:
-        a = bisect_right(self.calander,(start,end))
-        if (a>0 and self.calander[a-1][1]>start) or (a<len(self.calander) and self.calander[a][0]<end):
+        def find(node,lower,upper,start,end):
+            if start > upper or end < lower:
+                return 0
+            if start <= lower and end >= upper:
+                return self.seg[node]
+            mid = (lower+upper)//2
+            return self.lazy[node] + max(find(node*2,lower,mid,start,end), find(node*2+1,mid+1,upper,start,end))
+        def update(node,lower,upper,start,end):
+            if start > upper or end < lower:
+                return 0
+            if start <= lower and end >= upper:
+                self.seg[node] += 1
+                self.lazy[node] += 1
+                return self.seg[node]
+            else:
+                mid = (lower+upper)//2
+                self.seg[node] = self.lazy[node] + max(update(node*2,lower,mid,start,end),             update(node*2+1,mid+1,upper,start,end))
+            return self.seg[node]
+        if find(1,0,10**9+1,start,end-1) >= 1:
             return False
-        insort(self.calander,(start,end))
+        update(1,0,10**9+1,start,end-1)
         return True
 
 
