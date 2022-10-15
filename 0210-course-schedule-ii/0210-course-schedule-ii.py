b81cb08f2ef744b17eb12a2d3a336977e6c07aab
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-15 03:53:19 +0300

    Time: 104 ms (94.23%), Space: 17.6 MB (11.04%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,23 +1,26 @@
 class Solution:
     def findOrder(self, c: int, pre: List[List[int]]) -> List[int]:
-        graph = defaultdict(int)
-        out_going = defaultdict(list)
-        for i,v in pre:
-            graph[i] += 1
-            out_going[v].append(i)
-        deq = deque()
-        for i in range(c):
-            if not graph[i]:
-                deq.append(i)
+        graph = defaultdict(list)
+        for i,j in pre:
+            graph[i].append(j)
+        vstd = set()
         ans = []
-        while deq:
-            curr = deq.popleft()
-            for i in out_going[curr]:
-                graph[i] -= 1
-                if graph[i] == 0:
-                    deq.append(i)
-            ans.append(curr)
-        if len(ans) == c:
-            return ans
-        else: return []
-            
+        def dfs(node):
+            if node in cycle:
+                return False
+            if node in vstd:
+                return True
+            cycle.add(node)
+            for i in graph[node]:
+                if not dfs(i):
+                    return False
+            cycle.remove(node)
+            vstd.add(node)
+            ans.append(node)
+            return True
+        for i in range(c):
+            cycle = set()
+            if not dfs(i):
+                return []
+        return ans
+        
