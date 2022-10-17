6ef1257bbf993259fe4a187ee0db28d77cdfbd5b
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-17 14:19:27 +0300

    Time: 203 ms (7.25%), Space: 18.6 MB (26.13%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -7,10 +7,14 @@ class Solution:
             if state == 1:
                 ans = 0
                 for i in range(idx,len(prices)):
+                    if prices[idx]>=prices[i] and i-idx>3:
+                        break
                     ans = max(ans,prices[i]+dp(i+2,0))
             else:
                 ans = 0
                 for i in range(idx,len(prices)):
+                    if prices[idx]<=prices[i] and i-idx>3:
+                        break
                     ans = max(ans,-prices[i]+dp(i+1,1))
             return ans
         return dp(0,0)
