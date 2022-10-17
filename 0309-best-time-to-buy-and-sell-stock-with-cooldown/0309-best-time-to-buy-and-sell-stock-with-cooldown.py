1b5b32f5423e6426e18ba6f511053af67c7dd3d8
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-17 14:33:14 +0300

    Time: 77 ms (58.58%), Space: 14.8 MB (47.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,23 +1,11 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        memo = {}
-        def dp(idx,state):
-            if idx>=len(prices):
-                return 0
-            if (idx,state) in memo:
-                return memo[(idx,state)]
-            if state == 1:
-                ans = 0
-                for i in range(idx,len(prices)):
-                    if prices[idx]>=prices[i] and i-idx>3:
-                        break
-                    ans = max(ans,prices[i]+dp(i+2,0))
-            else:
-                ans = 0
-                for i in range(idx,len(prices)):
-                    if prices[idx]<=prices[i] and i-idx>3:
-                        break
-                    ans = max(ans,-prices[i]+dp(i+1,1))
-            memo[(idx,state)] = ans
-            return ans
-        return dp(0,0)
+class Solution(object):
+    def maxProfit(self, prices):
+        n = len(prices)
+        dp = [[0, 0, 0] for _ in range(n)]
+        dp[0][0] = -prices[0]
+        
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][2] - prices[i])
+            dp[i][1] = dp[i-1][0] + prices[i]
+            dp[i][2] = max(dp[i-1][1], dp[i-1][2])
+        return max(dp[-1][1], dp[-1][2])
