3770e923ed59753f585343d02cab417e0259cba0
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-19 08:54:07 +0300

    Time: 188 ms (7.39%), Space: 15 MB (46.66%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,6 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = defaultdict(int)
+        memo = [[0 for i in range(2)] for j in range(len(prices)+2)]
+        def dp(idx,state):
+            if idx>=len(prices):
+                return 0
+            if (idx,state) in memo:
+                return memo[(idx,state)]
+            if state == 1:
+                ans = 0
+                for i in range(idx,len(prices)):
+                    if prices[idx]>=prices[i] and i-idx>3:
+                        break
+                    ans = max(ans,prices[i]+dp(i+2,0))
+            else:
+                ans = 0
+                for i in range(idx,len(prices)):
+                    if prices[idx]<=prices[i] and i-idx>3:
+                        break
+                    ans = max(ans,-prices[i]+dp(i+1,1))
+            memo[(idx,state)] = ans
+            return ans
+        
         for idx in range(len(prices)-1,-1,-1):
             for state in [0,1]:
                 if state == 1:
@@ -8,13 +28,13 @@ class Solution:
                     for i in range(idx,len(prices)):
                         if prices[idx]>=prices[i] and i-idx>3:
                             break
-                        ans = max(ans,prices[i]+memo[(i+2,0)])
+                        ans = max(ans,prices[i]+memo[i+2][0])
                 else:
                     ans = 0
                     for i in range(idx,len(prices)):
                         if prices[idx]<=prices[i] and i-idx>3:
                             break
-                        ans = max(ans,-prices[i]+memo[(i+1,1)])
-                memo[(idx,state)] = ans
+                        ans = max(ans,-prices[i]+memo[i+1][1])
+                memo[idx][state] = ans
 
-        return memo[(0,0)]
+        return memo[0][0]
