acf7f0ce6fa6899441b7c0863c335eb41b8fdca3
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-19 14:38:09 +0300

    Time: 1534 ms (12.59%), Space: 26.4 MB (6.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,18 +1,23 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @lru_cache(None)
+        memo = [-1]*len(nums)
         def dp(idx):
             if idx == len(nums)-1:
                 return True
             elif idx>=len(nums):
                 return False
+            if memo[idx] != -1:
+                return memo[idx]
             curr = idx+nums[idx]
             for i in range(idx+1,min(len(nums),idx+nums[idx]+1)):
                 if i+nums[i] > curr:
                     curr = i+nums[i] 
                     if dp(i):
+                        memo[idx] = True
                         return True
                 elif i == len(nums)-1:
+                    memo[idx] = True
                     return True
+            memo[idx] = False
             return False
         return dp(0)
