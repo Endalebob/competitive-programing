1c67a6e7c692ffd75ea15e9a2172b4114896ea86
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-10-31 15:19:06 +0300

    Time: 9715 ms (5.15%), Space: 31.4 MB (32.04%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,33 +1,71 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.rank = [1 for i in range(n)]
+        self.num_dis_set = n
+
+    def find(self, xp):
+        compress = []
+        while xp != self.parents[xp]:
+            compress.append(xp)
+            xp = self.parents[xp]
+
+        for i in compress:
+            self.parents[i] = xp
+
+        return xp
+
+    def union(self, a, b):
+        ap, bp = self.find(a), self.find(b)
+
+        if ap == bp:
+            return
+
+        if self.rank[ap] < self.rank[bp]:
+            ap, bp = bp, ap
+        self.parents[bp] = ap
+        self.rank[ap] += self.rank[bp]
+
+        self.num_dis_set -= 1
+
+    def size(self, x):
+        return self.rank[self.find(x)]
+
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        block = defaultdict(int)
-        blocked = defaultdict(set)
         n = len(grid)
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
-        isvalid = lambda r,c : 0<=r<n and 0<=c<n
-        vstd = set()
-        idd = 0
-        def dfs(i,j,idd):
-            block[idd] += 1
-            vstd.add((i,j))
-            for ii,jj in directions:
-                r,c = ii+i,jj+j
-                if isvalid(r,c) and (r,c) not in vstd:
-                    if grid[r][c] == 0:
-                        blocked[(r,c)].add(idd)
-                    else:
-                        dfs(r,c,idd)
+        uf = UnionFind(n*n)
+        directions = [[0, 1], [1, 0]]
+        isvalid = lambda r, c: 0 <= r < n and 0 <= c < n
         for i in range(n):
             for j in range(n):
-                if grid[i][j] == 1 and (i,j) not in vstd:
-                    idd += 1
-                    dfs(i,j,idd)
-        if not blocked and grid[0][0] == 1:
-            return n*n
-        ans = 0
-        for key in blocked:
-            temp = 0
-            for j in blocked[key]:
-                temp += block[j]
-            ans = max(ans,temp)
+                if grid[i][j] == 1:
+                    for dx,dy in directions:
+                        r,c = dx+i,dy+j
+                        if isvalid(r,c) and grid[r][c] == 1:
+                            uf.union(r*n+c,i*n+j)
+        directions = [[0, 1], [1, 0],[-1,0],[0,-1]]
+        ans = uf.size(0)-1
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    vstd = set()
+                    temp = 0
+                    for dx,dy in directions:
+                        r,c = dx+i,dy+j
+                        if isvalid(r,c) and grid[r][c] == 1:
+                            find = uf.find(r*n+c)
+                            if find not in vstd:
+                                vstd.add(find)
+                                temp += uf.size(find)
+                    ans = max(ans,temp)
         return ans+1
+                            
+        
+        
+        
+        
+        
+        
+        
+        
