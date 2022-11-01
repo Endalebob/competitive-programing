c814a30b0deac28d7b636bb0f4bc607baf0c30b9
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-01 20:00:20 +0300

    Time: 166 ms (47.26%), Space: 15.8 MB (5.63%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,7 +1,7 @@
 class Solution:
     def countBattleships(self, matrix: List[List[str]]) -> int:
         Component_count = 0
-        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        directions = [[0,1],[1,0]]
         row = len(matrix)
         col = len(matrix[0])
 
