be75797ea12ead7d732666fd8c7a831c01596944
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-01 20:04:09 +0300

    Time: 235 ms (5.53%), Space: 14.7 MB (21.09%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -2,20 +2,20 @@ class Solution:
     def countBattleships(self, matrix: List[List[str]]) -> int:
         Component_count = 0
         directions = [[0,1],[1,0]]
+        
         row = len(matrix)
         col = len(matrix[0])
 
         isvalid = lambda r,c : 0<=r<row and 0<=c<col and matrix[r][c] == 'X'
         def dfs(i,j):
-            visited.add((i,j))
+            matrix[i][j] = '.'
             for xi,yi in directions:
                 r,c = i+xi,j+yi
-                if isvalid(r,c) and (r,c) not in visited:
+                if isvalid(r,c) and (r,c):
                     dfs(r,c)
-        visited = set()
         for r in range(row):
             for c in range(col):
-                if matrix[r][c] == 'X' and (r,c) not in visited:
+                if matrix[r][c] == 'X' and (r,c):
                     dfs(r,c)
                     Component_count = Component_count+1
         return Component_count
