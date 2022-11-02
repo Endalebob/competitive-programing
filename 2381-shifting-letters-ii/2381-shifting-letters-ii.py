fe7cc02c389ad68dfd5f51cfa09e53a7d7609e46
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-02 05:06:29 +0300

    Time: 1428 ms (88.80%), Space: 39.6 MB (24.56%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        pref_sum = [ord(i) for i in s]
+        start = defaultdict(int)
+        end = defaultdict(int)
+        for l,r,flag in shifts:
+            if flag:
+                start[l] += 1
+                end[r+1] += 1
+            else:
+                start[l] -= 1
+                end[r+1] -= 1
+        current = 0
+        for i in range(len(s)):
+            current += (start[i]-end[i])
+            pref_sum[i] += current
+        print(pref_sum)
+        return ''.join([chr((i-97)%26 + 97) for i in pref_sum])
+            
+            
+                
