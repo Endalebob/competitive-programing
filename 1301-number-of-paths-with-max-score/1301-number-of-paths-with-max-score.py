d73c605cebdad27de3a603a9bfaa9a8a4c91d1b0
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-02 12:57:01 +0300

    Time: 604 ms (19.51%), Space: 24.8 MB (12.20%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.py
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.py
@@ -6,26 +6,22 @@ class Solution:
         @lru_cache(None)
         def dp(i,j):
             if i == row-1 and j == col-1:
-                return {0:1}
-            ans = {-float('inf'):0}
+                return [0,1]
+            ans = [-float('inf'),0]
             for ir,ic in dirc:
                 r,c = ir+i,ic+j
                 if isvalid(r,c):
-                    temp = dp(r,c)
-                    tempkey = list(temp.keys())
-                    anskey = list(ans.keys())
-                    if tempkey[0] > anskey[0]:
-                        ans = {tempkey[0]:temp[tempkey[0]]}
-                    elif tempkey[0] == anskey[0]:
-                        ans[anskey[0]] += temp[tempkey[0]]
-            anskey = list(ans.keys())
+                    tempkey = dp(r,c)
+                    if tempkey[0] > ans[0]:
+                        ans = tempkey
+                    elif tempkey[0] == ans[0]:
+                        ans[1] += tempkey[1]
             if i+j > 0:
-                return {anskey[0]+int(board[i][j]):ans[anskey[0]]}
+                return [ans[0]+int(board[i][j]),ans[1]]
             return ans
         val = dp(0,0)
-        m = list(val.keys())
-        if m[0] < 0:
+        if val[0] < 0:
             return [0,0]
-        return [m[0],val[m[0]]%(10**9+7)]
+        return [val[0],val[1]%(10**9+7)]
                             
                 
