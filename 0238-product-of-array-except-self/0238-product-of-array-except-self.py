ebe51db97ddc9602da0c51f0cc2616043bb79f6d
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-02 21:31:22 +0300

    Time: 973 ms (5.01%), Space: 21.4 MB (58.70%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,11 +1,47 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        right,left = [1],[1]
-        for i in nums:
-            left.append(left[-1]*i)
-        for i in nums[::-1]:
-            right.append(right[-1]*i)
-        right = right[::-1]
-        for i in range(len(nums)):
-            nums[i] = right[i+1]*left[i]
-        return nums
+        zeros_index = -1
+        product = 1
+        count = 0
+        for index,num in enumerate(nums):
+            if num == 0:
+                count +=1
+                zeros_index = index
+            else:
+                product *=num
+        
+        if count > 1:
+            return [0 for _ in range(len(nums))]
+        
+        if count == 1:
+            return [0 if index != zeros_index else product for index in range(len(nums))]
+        
+        output = []
+        
+        for num in nums:
+            tempo_res = 0
+            devidend = abs(product)
+            devisor = abs(num)
+            
+            while devidend:
+                multiplier = 1
+                
+                while devidend - multiplier * devisor >= 0:
+                    tempo_res += multiplier
+                    devidend -= multiplier*devisor
+                    multiplier *=2
+            if num < 0 and product < 0 or num > 0 and product > 0:
+                output.append(tempo_res)
+            else:
+                output.append(-tempo_res)
+            
+        
+        return output
+                
+            
+                
+                
+        
+        
+        
+                
