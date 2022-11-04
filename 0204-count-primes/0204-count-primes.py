70d160027b1470ca05f4ad363b7496a8912fd9cf
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-04 14:30:36 +0300

    Time: 3473 ms (73.70%), Space: 52.8 MB (84.69%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -4,8 +4,7 @@ class Solution:
             return 0
         prime = [1]*(n)
         for j in range(2,int(n**0.5)+1):
-            if prime[j] == 0:
-                continue
-            for i in range(j*j,n,j):
-                prime[i] = 0
+            if prime[j]:
+                for i in range(j*j,n,j):
+                    prime[i] = 0
         return prime.count(1)-2
