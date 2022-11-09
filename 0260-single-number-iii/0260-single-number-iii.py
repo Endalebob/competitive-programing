be0e87f91c5d812e107fcd60db7d82396053bd93
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-09 10:52:15 +0300

    Time: 124 ms (62.26%), Space: 16.3 MB (16.64%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,11 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        xor = 0
-        for i in nums:
-            xor ^= i
-        last_bit_of_xor = xor & (xor-1) ^ xor
-        n1 = 0
-        for i in nums:
-            if last_bit_of_xor & i:
-                n1 ^= i
-        return n1,n1^xor
+        dic = Counter(nums)
+        ans = []
+        for i in dic:
+            if dic[i] == 1:
+                ans.append(i)
+        return ans
