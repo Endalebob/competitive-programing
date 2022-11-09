b631075a1214560fedf60e2c354a2eeb85ef9492
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-09 19:17:18 +0300

    Time: 4618 ms (5.01%), Space: 23 MB (8.09%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,54 +1,36 @@
-class Node:
-    def __init__(self):
-        self.letters = [None] * 26
-        self.is_end = False
-
-
 class Trie:
     def __init__(self):
-        self.root = Node()
+        self.letters = {}
+        self.is_end = False
 
     def insert(self, word):
-        val_of_a = ord('a')
-        current = self.root
-
+        current = self
         for letter in word:
-            letter = ord(letter) - val_of_a
-            if current.letters[letter] is None:
-                current.letters[letter] = Node()
+            if letter not in current.letters:
+                current.letters[letter] = Trie()
             current = current.letters[letter]
         current.is_end = True
 
-    def find(self, word):
-
-        val_of_a = ord('a')
-        current = self.root
+    def search(self, word):
+        current = self
         for letter in word:
-            letter = ord(letter) - val_of_a
-            if current.letters[letter] is None:
+            if letter not in current.letters:
                 return False
             current = current.letters[letter]
-
         return current.is_end
 
-    def is_prefix(self, word):
-        val_of_a = ord('a')
-        current = self.root
-        for letter in word:
-            letter = ord(letter) - val_of_a
-            if current.letters[letter] is None:
+    def prefix(self, pref):
+        current = self
+        for letter in pref:
+            if letter not in current.letters:
                 return False
             current = current.letters[letter]
-
         return True
 
     def auto_complete(self, pref):
-        val_of_a = ord('a')
-        current = self.root
-
+        current = self
         for letter in pref:
-            letter = ord(letter) - val_of_a
-            if current.letters[letter] is None:
+            if letter not in current.letters:
                 return []
             current = current.letters[letter]
 
@@ -57,19 +39,16 @@ class Trie:
         def dfs(current, pref):
             if current.is_end:
                 words.append(pref)
-            if len(words) >= 3:
-                return
-            if current is None:
-                return
             for i in range(26):
                 if len(words) >= 3:
                     return
-                if current.letters[i]:
-                    lett = chr(ord('a') + i)
-                    dfs(current.letters[i], pref + lett)
-        dfs(current,pref)
+                letter = chr(i + ord('a'))
+                if letter in current.letters:
+                    dfs(current.letters[letter], pref + letter)
 
+        dfs(current, pref)
         return words
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         trie = Trie()
