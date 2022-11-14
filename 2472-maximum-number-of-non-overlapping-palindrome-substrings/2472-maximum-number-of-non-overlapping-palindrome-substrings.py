f984b688213c39a5ad2741894972129e7d488d05
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-11-14 05:59:20 +0300

    Time: 178 ms (75.00%), Space: 24.1 MB (25.00%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -13,10 +13,7 @@ class Solution:
         def dp(l,r):
             if r >= len(s) or r-l > k:
                 return 0
-            if l in memo:
-                return memo[l]
             if ispal(l,r):
-                memo[l] =  1+dp(r+1,r+k)
-                return memo[l]
+                return 1+dp(r+1,r+k)
             return max(dp(l+1,r+1),dp(l,r+1))
         return dp(0,k-1)
