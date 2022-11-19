e2bb6f5375972dab5ea293c2a09e0f4007055575
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-19 19:04:24 +0300

    Time: 2865 ms (5.73%), Space: 292 MB (7.00%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,9 +1,11 @@
 class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
-        
-        for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] += dp[x - coin]
-        return dp[amount]
+    def change(self, a: int, c: List[int]) -> int:
+        vstd = set()
+        @lru_cache(None)
+        def dp(s,idx):
+            if s == a:
+                return 1
+            if idx >= len(c) or s>a:
+                return 0
+            return dp(s+c[idx],idx)+dp(s,idx+1)
+        return dp(0,0)
