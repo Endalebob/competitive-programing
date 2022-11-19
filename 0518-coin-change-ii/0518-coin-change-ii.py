70b19a4245f8dd5eed4b79795e9d04151ddff4e7
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-19 18:43:45 +0300

    Time: 331 ms (78.57%), Space: 14 MB (90.58%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,14 +1,9 @@
 class Solution:
-    def change(self, a: int, c: List[int]) -> int:
-        vstd = set()
-        memo = {}
-        def dp(s,idx):
-            if s == a:
-                return 1
-            if idx >= len(c) or s>a:
-                return 0
-            if (s,idx) in memo:
-                return memo[(s,idx)]
-            memo[(s,idx)] = dp(s+c[idx],idx)+dp(s,idx+1)
-            return memo[(s,idx)]
-        return dp(0,0)
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
+        return dp[amount]
