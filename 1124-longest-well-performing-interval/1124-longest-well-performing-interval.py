c051c4f07f0d8baaedd8c040e4e94239efb46570
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-19 18:13:48 +0300

    Time: 667 ms (14.42%), Space: 14.6 MB (51.63%) - LeetHub

---
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestWPI(self, hours: List[int]) -> int:
+        
+        
+        #go by creating prefix_sum if it is postive add upto index
+        #else check is there any other number less than current if exit update
+        
+        dic = {}
+        score = ans = 0
+        for i,v in enumerate(hours):
+            score = score + 1 if v>8 else score - 1
+            
+            if score > 0:
+                ans = i+1
+            if score not in dic:
+                dic[score] = i
+            if score-1 in dic:
+                ans = max(ans,i-dic[score-1])
+        
+        return ans
