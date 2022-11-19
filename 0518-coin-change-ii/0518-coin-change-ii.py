7b5f75fddd2f509520e64945bafa7fe8623beed8
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-19 21:22:17 +0300

    Time: 3313 ms (5.00%), Space: 59.5 MB (17.29%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,11 +1,17 @@
 class Solution:
     def change(self, a: int, c: List[int]) -> int:
         vstd = set()
+        c.sort()
         @lru_cache(None)
         def dp(s,idx):
             if s == a:
                 return 1
-            if idx >= len(c) or s>a:
+            if s>a:
                 return 0
-            return dp(s+c[idx],idx)+dp(s,idx+1)
+            ans = 0
+            for i in range(idx,len(c)):
+                if c[i]+s > a:
+                    break
+                ans += dp(s+c[i],i)
+            return ans
         return dp(0,0)
