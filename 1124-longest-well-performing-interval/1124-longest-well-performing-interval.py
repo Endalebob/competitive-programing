25e0b4b40369b2e93f1ce9ee93421ea826cc78e8
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-19 18:17:47 +0300

    Time: 573 ms (34.42%), Space: 14.7 MB (51.63%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -7,12 +7,14 @@ class Solution:
         
         dic = {}
         score = ans = 0
+        
+        
         for i,v in enumerate(hours):
             score = score + 1 if v>8 else score - 1
             
             if score > 0:
                 ans = i+1
-            if score not in dic:
+            elif score not in dic:
                 dic[score] = i
             if score-1 in dic:
                 ans = max(ans,i-dic[score-1])
