4e741c7f21d0cb3c987c36f482045af9ebe3a726
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-19 06:06:44 +0300

    Time: 4049 ms (31.10%), Space: 36.9 MB (10.73%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,14 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = [-9]*amount
+        @lru_cache(None)
         def knapsack(s):
             if s == amount:
                 return 0
             elif s>amount:
                 return float('inf')
-            if memo[s] != -9:
-                return memo[s]
             ans = float('inf')
             for i in coins:
                 ans = min(knapsack(s+i),ans)
-            memo[s] = ans + 1
             return ans + 1
         ret = knapsack(0)
         return ret if ret != float('inf') else -1
