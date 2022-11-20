41bafd03e05353dab6400f1fe725ee24c4b2cde9
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-20 15:54:53 +0300

    Time: 62 ms (38.73%), Space: 14.1 MB (10.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        isvalid = lambda r,c : 0<=r<n and 0<=c<m
-        dirc = [[0,-1],[-1,0]]
-        dp = [[0 for i in range(m)] for j in range(n)]
-        dp[0][0] = 1
-        for i in range(n):
-            for j in range(m):
-                for ii,jj in dirc:
-                    r,c = ii+i,j+jj
-                    if isvalid(r,c):
-                        dp[i][j] += dp[r][c]
-        return dp[n-1][m-1]
+        @lru_cache(None)
+        def dp(r,c):
+            if r == m-1 and c == n-1:
+                return 1
+            if r >= m or c >= n:
+                return 0
+            return dp(r+1,c) + dp(r,c+1)
+        return dp(0,0)
