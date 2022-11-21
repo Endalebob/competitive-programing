865d76475bab59d6ed04dfe0325659c7e9885db7
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-21 13:30:46 +0300

    Time: 2588 ms (52.77%), Space: 41.5 MB (72.86%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,12 +1,24 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
-        start,end = [],[]
+        pref = defaultdict(int)
         for i,j in flowers:
-            start.append(i)
-            end.append(j)
-        start.sort()
-        end.sort()
+            pref[i] += 1
+            pref[j+1] -= 1
+        key = sorted(pref.keys())
+        curr = 0
+        for i in key:
+            curr += pref[i]
+            pref[i] = curr
         ans = []
         for i in persons:
-            ans.append(bisect_right(start,i)-bisect_left(end,i))
+            if i > key[-1]:
+                ans.append(0)
+                continue
+            idx = bisect_left(key,i)
+            if key[idx] == i:
+                
+                ans.append(pref[key[idx]])
+            else:
+                ans.append(pref[key[idx-1]])
         return ans
+                            
