eb863ec78cd27ac41a36da67936ebf564a3b48e3
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-22 06:21:38 +0300

    Time: 1765 ms (91.80%), Space: 14 MB (48.69%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -4,22 +4,27 @@ class Solution:
         
         is_valid = lambda r,c : 0<=r<n and 0<=c<m and grid[r][c] != 0
         dirc = [[0,1],[1,0],[-1,0],[0,-1]]
-        
-        def dfs(r,c):
+        def dfs(r,c,cnt):
+            self.tot = max(self.tot,cnt)
             ans = 0
             for dx,dy in dirc:
                 new_x,new_y = dx+r,dy+c
-                if is_valid(new_x,new_y) and (new_x,new_y) not in vstd:
-                    vstd.add((new_x,new_y))
-                    ans = max(ans,grid[new_x][new_y]+dfs(new_x,new_y))
-                    vstd.remove((new_x,new_y))
+                if is_valid(new_x,new_y):
+                    original = grid[new_x][new_y]
+                    grid[new_x][new_y] = 0
+                    ans = max(ans,original+dfs(new_x,new_y,cnt+1))
+                    grid[new_x][new_y] = original
             return ans
+        
         ret = 0
-        vstd = set()
         for i in range(n):
             for j in range(m):
                 if grid[i][j]:
-                    vstd.add((i,j))
-                    ret = max(ret,grid[i][j]+dfs(i,j))
-                    vstd.remove((i,j))
+                    self.tot = 0
+                    ori = grid[i][j]
+                    grid[i][j] = 0
+                    ret = max(ret,ori+dfs(i,j,1))
+                    grid[i][j] = ori
+                    if self.tot == min(m*n,25):
+                        return ret
         return ret
