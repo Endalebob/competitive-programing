1c473f57b474d354a0845a6e36a038df5d901f33
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-23 05:17:33 +0300

    Time: 5916 ms (11.23%), Space: 67.4 MB (92.02%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        graph = defaultdict(list)
+        in_degree = defaultdict(int)
+        cnt = defaultdict(int)
+        for i,j in roads:
+            graph[i].append(j)
+            graph[j].append(i)
+            in_degree[i] += 1
+            in_degree[j] += 1
+        
+        deq = deque()
+        for i in range(1,len(roads)+1):
+            if in_degree[i] == 1:
+                deq.append(i)
+                cnt[i] += 1
+        ans = 0
+        while deq:
+            node = deq.popleft()
+            ans += int(ceil(cnt[node]/seats))
+            for i in graph[node]:
+                in_degree[i] -= 1
+                cnt[i] += cnt[node]
+                if in_degree[i] == 1:
+                    if i != 0:
+                        cnt[i] += 1
+                        deq.append(i)
+        return ans
+                        
