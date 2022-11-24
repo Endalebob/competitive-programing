3527c854c053ae02ba447b00e5b73cf78bac55d8
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-24 07:09:42 +0300

    Time: 5629 ms (17.41%), Space: 24 MB (12.91%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,7 +1,18 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        heapq.heapify(nums)
-        ans = []
-        while nums:
-            ans.append(heapq.heappop(nums))
-        return ans
+        def merge_sort(collection):
+            def merge(left: list, right: list) -> list:
+                
+                def _merge():
+                    while left and right:
+                        yield (left if left[0] <= right[0] else right).pop(0)
+                    yield from left
+                    yield from right
+
+                return list(_merge())
+
+            if len(collection) <= 1:
+                return collection
+            mid = len(collection) // 2
+            return merge(merge_sort(collection[:mid]), merge_sort(collection[mid:]))
+        return merge_sort(nums)
