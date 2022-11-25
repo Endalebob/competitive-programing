57e6e54986fedbbcb025346b5a3482343d9b713b
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-25 20:19:05 +0300

    Time: 1518 ms (97.09%), Space: 118.3 MB (49.03%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,7 +1,7 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         n,m = len(grid),len(grid[0])
-        isvalid = lambda r,c : 0<=r<n and 0<=c<m
+        isvalid = lambda r,c : 0<=r<n and 0<=c<m and grid[r][c] != 0
         dirc = {
             1: [(0, -1), (0, 1)] ,
             2: [(-1, 0), (1, 0)] ,
@@ -13,14 +13,16 @@ class Solution:
         
         vstd =set()
         def dfs(r,c):
-            vstd.add((r,c))
+            temp = grid[r][c]
             if r == n-1 and c == m-1:
                 return True
             di = dirc[grid[r][c]]
+            grid[r][c] = 0
             for dr,dc in di:
                 rr,cc = r+dr,dc+c
-                if isvalid(rr,cc) and (rr,cc) not in vstd and (-dr, -dc) in dirc[grid[rr][cc]]:
+                if isvalid(rr,cc) and (-dr, -dc) in dirc[grid[rr][cc]]:
                     if dfs(rr,cc):
                         return True
+            grid[r][c] = temp
             return False
         return dfs(0,0)
