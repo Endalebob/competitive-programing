385c96c2f8fa6079c3084241510c2f2754b2f958
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-11-26 06:43:30 +0300

    Time: 1436 ms (79.25%), Space: 97 MB (36.79%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
@@ -1,7 +1,7 @@
 class Solution:
     def hasValidPath(self, grid: List[List[str]]) -> bool:
         n,m = len(grid),len(grid[0])
-        if grid[0][0] != '(' or grid[n-1][m-1] != ')':
+        if grid[0][0] != '(' or grid[n-1][m-1] != ')' or (m+n) % 2 == 0:
             return False
         is_valid = lambda r,c : r<n and c < m
         @lru_cache(None)
