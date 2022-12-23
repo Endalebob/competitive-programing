d7c36eea3565db040389bf8099bc22a813a0815d
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-12-23 18:13:25 +0300

    Time: 75 ms (62.04%), Space: 18.4 MB (25.99%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,40 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = [[0 for i in range(2)] for j in range(len(prices)+2)]
-        def dp(idx,state):
-            if idx>=len(prices):
+        @lru_cache(None)
+        def dp(idx,flag):
+            if idx >= len(prices):
                 return 0
-            if (idx,state) in memo:
-                return memo[(idx,state)]
-            if state == 1:
-                ans = 0
-                for i in range(idx,len(prices)):
-                    if prices[idx]>=prices[i] and i-idx>3:
-                        break
-                    ans = max(ans,prices[i]+dp(i+2,0))
-            else:
-                ans = 0
-                for i in range(idx,len(prices)):
-                    if prices[idx]<=prices[i] and i-idx>3:
-                        break
-                    ans = max(ans,-prices[i]+dp(i+1,1))
-            memo[(idx,state)] = ans
-            return ans
-        
-        for idx in range(len(prices)-1,-1,-1):
-            for state in [0,1]:
-                if state == 1:
-                    ans = 0
-                    for i in range(idx,len(prices)):
-                        if prices[idx]>=prices[i] and i-idx>3:
-                            break
-                        ans = max(ans,prices[i]+memo[i+2][0])
-                else:
-                    ans = 0
-                    for i in range(idx,len(prices)):
-                        if prices[idx]<=prices[i] and i-idx>3:
-                            break
-                        ans = max(ans,-prices[i]+memo[i+1][1])
-                memo[idx][state] = ans
-
-        return memo[0][0]
+            if flag:
+                return max(dp(idx+1,1-flag)-prices[idx],dp(idx+1,flag))
+            return max(dp(idx+2,1-flag)+prices[idx],dp(idx+1,flag))
+        return dp(0,1)
