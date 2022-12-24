ede5423fdeaa5762f456ad01c261eb85c10b8969
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-12-24 05:24:25 +0300

    Time: 670 ms (6.67%), Space: 15.9 MB (30.95%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,14 +1,13 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        mod = 10**9 + 7
         @lru_cache(None)
         def dp(n):
             if n == 0: return 1
             ans = 0
             for i in range(1,n+1):
-                if i < 3:
-                    ans += dp(n-i)
-                else:
+                if i >= 3:
                     ans += 2*dp(n-i)
-            return ans % mod
-        return dp(n)%mod
+                else:
+                    ans += dp(n-i)
+            return ans % (10**9+7)
+        return dp(n)
