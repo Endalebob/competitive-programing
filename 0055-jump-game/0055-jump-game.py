1702800636dc066e905f13216c98b6247a578ec6
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2022-12-26 05:24:40 +0300

    Time: 607 ms (73.72%), Space: 32.8 MB (5.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,8 +1,18 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        mTrue = len(nums)-1
-        for idx in range(len(nums)-2,-1,-1):
-            if idx + nums[idx] >= mTrue:
-                mTrue = idx
-        return mTrue == 0
-        
+        @lru_cache(None)
+        def dp(idx):
+            if idx == len(nums)-1:
+                return True
+            elif idx>=len(nums):
+                return False
+            curr = idx+nums[idx]
+            for i in range(idx+1,min(len(nums),idx+nums[idx]+1)):
+                if i+nums[i] > curr:
+                    curr = i+nums[i] 
+                    if dp(i):
+                        return True
+                elif i == len(nums)-1:
+                    return True
+            return False
+        return dp(0)
