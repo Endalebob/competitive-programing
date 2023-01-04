ad1541d2957fd44672bc6a3d56176568330f8ada
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-04 10:06:47 +0300

    Time: 1021 ms (77.05%), Space: 28.5 MB (44.10%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,15 +1,9 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        c = Counter(tasks)
-        nums = list(c.values())
-        @lru_cache(None)
-        def dp(curr,idx):
-            if idx == len(nums)-1 and curr == 0:
-                return 0
-            if curr < 0:
-                return float('inf')
-            if curr == 0:
-                return dp(nums[idx+1],idx+1)
-            return min(dp(curr-3,idx)+1,dp(curr-2,idx)+1)
-        ans = dp(nums[0],0)
-        return ans if ans < float('inf') else -1
+        count = Counter(tasks)
+        ans = 0
+        for i in count:
+            if count[i] == 1:
+                return -1
+            ans += ceil(count[i]/3)
+        return ans
