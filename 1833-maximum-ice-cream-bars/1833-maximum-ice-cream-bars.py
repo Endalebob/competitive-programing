ea115a54d2f2cd512da6e97b79d8d3153d3b0f61
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-06 21:54:21 +0300

    Time: 864 ms (82.20%), Space: 27.9 MB (94.50%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,5 +1,11 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
+        '''
+        1: I sort the costs because I need to start from low price
+        2: I continue until my the sum of visited index is less than
+            coins
+        3: finally I return the number of visited index
+        '''
         costs.sort()
         cnt = 0
         for i in costs:
