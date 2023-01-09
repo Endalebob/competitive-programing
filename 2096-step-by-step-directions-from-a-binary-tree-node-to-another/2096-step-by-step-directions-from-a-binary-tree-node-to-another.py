e05698820b2dbf8e43ec05a24b1e8f936c5e5d29
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-09 14:49:56 +0300

    Time: 1394 ms (62.00%), Space: 69.8 MB (75.27%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -5,29 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def getDirections(self, root: Optional[TreeNode], s: int, t: int) -> str:
-        def rec(r):
-            if not r:
-                return [False,False,'']
-            right = rec(r.right)
-            left = rec(r.left)
-            f,l = '',''
-            if right[0] and right[1]:
-                return right
-            if left[0] and left[1]:
-                return left
-            if right[0]:
-                f = right[2]+'U'
-            if right[1]:
-                l = 'R' + right[2]
-            if left[0]:
-                f = left[2]+'U'
-            if left[1]:
-                l = 'L'+left[2]
-            val = [right[0] | left[0],right[1] | left[1]]
-            if r.val == s:
-                val[0] = True
-            elif r.val == t:
-                val[1] = True
-            return [val[0],val[1],f+l]
-        return rec(root)[2]
+    def getDirections(self, root: Optional[TreeNode], s: int, d: int) -> str:
+        graph = defaultdict(list)
+        deq = deque([root])
+        while deq:
+            curr = deq.popleft()
+            if curr.left:
+                graph[curr.left.val].append((curr.val,'U'))
+                graph[curr.val].append((curr.left.val,'L'))
+                deq.append(curr.left)
+            if curr.right:
+                graph[curr.right.val].append((curr.val,'U'))
+                graph[curr.val].append((curr.right.val,'R'))
+                deq.append(curr.right)
+        deq = deque([(s,'')])
+        vstd = {s}
+        while deq:
+            node,path = deq.popleft()
+            if node == d:
+                return path
+            for i,j in graph[node]:
+                if i not in vstd:
+                    vstd.add(i)
+                    deq.append((i,path+j))
+            
