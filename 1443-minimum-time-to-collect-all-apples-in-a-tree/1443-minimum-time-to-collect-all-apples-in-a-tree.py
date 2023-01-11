43f0690f250d2c49e160dbf611ce3efa8ee176e0
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-11 04:17:05 +0300

    Time: 739 ms (75.00%), Space: 54.4 MB (47.44%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -5,14 +5,14 @@ class Solution:
             graph[i].append(j)
             graph[j].append(i)
         vstd = set()
-        def dfs(node,depth):
+        def dfs(node):
             vstd.add(node)
             ans = 0
             for i in graph[node]:
                 if i not in vstd:
-                    ans += dfs(i,depth+1)
+                    ans += dfs(i)
             if ans or hasApple[node]:
                 return ans + 2
             return ans
-        return max(0,dfs(0,0)-2)
+        return max(0,dfs(0)-2)
             
