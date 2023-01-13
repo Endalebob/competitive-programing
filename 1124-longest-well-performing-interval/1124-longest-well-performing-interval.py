de9575562087cbe77b57a500e0f176ad655d5262
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-13 22:18:10 +0300

    Time: 208 ms (90.53%), Space: 14.5 MB (79.88%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -1,22 +1,20 @@
+from collections import defaultdict
 class Solution:
-    def longestWPI(self, hours: List[int]) -> int:
-        
-        
-        #go by creating prefix_sum if it is postive add upto index
-        #else check is there any other number less than current if exit update
-        
-        dic = {}
-        score = ans = 0
-        
-        
-        for i,v in enumerate(hours):
-            score = score + 1 if v>8 else score - 1
-            
-            if score > 0:
+    def longestWPI(self, nums: List[int]) -> int:
+        # i was wrong in the first attempt it’s amazing I’m sure if this is the will of God I will pass even if they ask me segment tree with lazy propagation if it is not it is better not to pass wow uuuuuuu…. Yes
+
+        '''
+        Calculate the current score.
+        '''
+        _track = defaultdict(int)
+        _score = 0
+        ans = 0
+        for i in range(len(nums)):
+            _score = _score + 1 if nums[i] > 8 else _score - 1
+            if _score > 0:
                 ans = i+1
-            elif score not in dic:
-                dic[score] = i
-            if score-1 in dic:
-                ans = max(ans,i-dic[score-1])
-        
+            if _score not in _track:
+                _track[_score] = i
+            if _score - 1 in _track:
+                ans = max(ans,i-_track[_score-1])
         return ans
