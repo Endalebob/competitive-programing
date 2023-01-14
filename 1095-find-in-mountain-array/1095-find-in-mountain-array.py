60258d5e3efd35410d72b6bff05e73031f575b92
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-14 10:12:14 +0300

    Time: 31 ms (86.20%), Space: 14.8 MB (56.79%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -8,35 +8,40 @@
 
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        # the first step is finding the mountain
+        #the first step is to find the mountain
         l,r = 0,mountain_arr.length()-1
-        while l<r:
+        if r < 2:
+            return -1
+        while l < r:
             m = l + (r-l)//2
             if mountain_arr.get(m) < mountain_arr.get(m+1):
                 l = m+1
             else:
                 r = m
-        mountain = l
         
-        l,r = 0,mountain
+        #check in the first part
+        val = l
+        r = val
+        l = 0
         while l <= r:
             m = l + (r-l)//2
-            val = mountain_arr.get(m)
-            if val == target:
+            if mountain_arr.get(m) == target:
                 return m
-            elif val > target:
-                r = m-1
+            if mountain_arr.get(m) > target:
+                r = m -1
             else:
-                l = m+1
-        l,r = mountain,mountain_arr.length()-1
-        while l<=r:
-            m = l + (r-l)//2
-            val = mountain_arr.get(m)
-            if val == target:
-                return m
-            elif val<target:
-                r = m-1
-            else:
-                l = m+1
-        return -1
+                l = m + 1
         
+        #second check
+        
+        l = val+1
+        r = mountain_arr.length()-1
+        while l <= r:
+            m = l + (r-l)//2
+            if mountain_arr.get(m) == target:
+                return m
+            if mountain_arr.get(m) < target:
+                r = m -1
+            else:
+                l = m + 1 
+        return -1
