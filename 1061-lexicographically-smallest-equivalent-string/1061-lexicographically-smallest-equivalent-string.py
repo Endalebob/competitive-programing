a609d05240a92d352b5966b88c031748b40962c9
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-14 06:15:35 +0300

    Time: 173 ms (7.47%), Space: 14.3 MB (6.90%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        dic = defaultdict(set)
+        for i,j in zip(s1,s2):
+            dic[i].add(j)
+            dic[j].add(i)
+        ans = ''
+        def dfs(node):
+            if node in vstd:
+                return node
+            vstd.add(node)
+            ans = node
+            for i in dic[node]:
+                ans = min(dfs(i),ans)
+            return ans
+        for i in baseStr:
+            vstd = set()
+            ans += dfs(i)
+        return ans
+            
