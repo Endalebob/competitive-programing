632dd471d8e95d3ade524f533a6b3552849d7366
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-14 09:27:39 +0300

    Time: 746 ms (39.80%), Space: 15.4 MB (71.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,16 +1,22 @@
 class Solution:
+    def is_possible(self,speed,h,piles):
+        curr = 0
+        i = 0
+        while curr <= h and i<len(piles):
+            curr += math.ceil(piles[i]/speed)
+            i += 1
+        if curr <= h and i == len(piles):
+            return True
+        return False
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        piles.sort()
-        l,r = 1,piles[-1]
-        while l<r:
+        l, r = 1, max(piles)
+                
+        while l < r:
             m = l + (r-l)//2
-            cnt = 0
-            i = 0
-            while i < len(piles) and cnt <= h:
-                cnt += ceil(piles[i]/m)
-                i += 1
-            if cnt <= h:
+            if self.is_possible(m,h,piles):
                 r = m
             else:
                 l = m + 1
         return l
+            
+            
