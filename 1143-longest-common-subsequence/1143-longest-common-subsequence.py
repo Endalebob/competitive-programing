d22eced64a6342248e8154ce721f71259d354017
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-15 16:22:52 +0300

    Time: 1235 ms (18.60%), Space: 199.2 MB (5.23%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,16 +6,14 @@ class Solution:
         to delete from the second. I try both and I take the one that generate
         maximum LCS.
         '''
-        memo = {}
-        def dp(idx1,idx2):
-            if idx1 >= len(text1) or idx2 >= len(text2):
-                return 0
-            if (idx1,idx2) in memo:
-                return memo[(idx1,idx2)]
-            if text1[idx1] == text2[idx2]:
-                memo[(idx1,idx2)] = 1 + dp(idx1+1,idx2+1)
-            else:
-                memo[(idx1,idx2)] = max(dp(idx1+1,idx2),dp(idx1,idx2+1))
-            return memo[(idx1,idx2)]
-        return dp(0,0)
+        memo = defaultdict(int)
+        for idx1 in range(len(text1)-1,-1,-1):
+            for idx2 in range(len(text2)-1,-1,-1):
+                if text1[idx1] == text2[idx2]:
+                    memo[(idx1,idx2)] = 1 + memo[(idx1+1,idx2+1)]
+                else:
+                    memo[(idx1,idx2)] = max(memo[(idx1+1,idx2)],memo[(idx1,idx2+1)])
+        return memo[(0,0)]
+                
+                
         
