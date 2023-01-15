d19e5fa8886258331c48c712a5631863d54a60af
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-15 16:13:31 +0300

    Time: 1128 ms (22.45%), Space: 140.5 MB (13.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,10 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @lru_cache(None)
+        '''
+        if I find the same letter I add them if I didn't I have two options 
+        the first is to delete the letter from text1 and the second option is
+        to delete from the second. I try both and I take the one that generate
+        maximum LCS.
+        '''
+        memo = {}
         def dp(idx1,idx2):
-            if idx1 == len(text1) or idx2 == len(text2):
+            if idx1 >= len(text1) or idx2 >= len(text2):
                 return 0
+            if (idx1,idx2) in memo:
+                return memo[(idx1,idx2)]
             if text1[idx1] == text2[idx2]:
-                return 1 + (dp(idx1+1,idx2+1))
-            return max(dp(idx1+1,idx2),dp(idx1,idx2+1))
+                memo[(idx1,idx2)] = 1 + dp(idx1+1,idx2+1)
+            else:
+                memo[(idx1,idx2)] = max(dp(idx1+1,idx2),dp(idx1,idx2+1))
+            return memo[(idx1,idx2)]
         return dp(0,0)
+        
