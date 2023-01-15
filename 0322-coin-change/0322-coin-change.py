7caea6783780dc457c9c6c20a26407eb56a8fada
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-15 15:56:11 +0300

    Time: 1785 ms (59.55%), Space: 45.1 MB (5.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,30 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
-        def knapsack(s):
-            if s == amount:
+        '''
+        our goal is to make the total amount of money using a few coin.
+        there may be many posibilities of getting that amount of money
+        we check every possibilities and we take the one with few coin.
+        
+        while we are perfoming combination there may be some overlap
+        it isn't necessary to solve again again the same problem in order
+        not to solve the same problem again again we need to memorize which 
+        problem we already solved.
+        '''
+        memo = {}
+        def dp(tot):
+            if tot == amount:
                 return 0
-            elif s>amount:
+            if tot > amount:
                 return float('inf')
+            if tot in memo:
+                return memo[tot]
             ans = float('inf')
             for i in coins:
-                ans = min(knapsack(s+i),ans)
-            return ans + 1
-        ret = knapsack(0)
-        return ret if ret != float('inf') else -1
+                ans = min(1+dp(tot+i),ans)
+            memo[tot] = ans
+            return ans
+        num_coin = dp(0)
+        if num_coin == float('inf'):
+            return -1
+        return num_coin
+        
