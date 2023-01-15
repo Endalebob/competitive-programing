887a00638a1422f31b4b50acd1f16e8c672707d0
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-15 16:51:00 +0300

    Time: 34 ms (76.40%), Space: 14.1 MB (9.80%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,9 +2,9 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         @lru_cache(None)
         def dp(r,c):
+            if r == m or c == n:
+                return 0
             if r == m-1 and c == n-1:
                 return 1
-            if r >= m or c >= n:
-                return 0
             return dp(r+1,c) + dp(r,c+1)
         return dp(0,0)
