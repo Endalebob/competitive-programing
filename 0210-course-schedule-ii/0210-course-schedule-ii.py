5a1eddbf5f238713d6b12a07d84a90c69e60d1bf
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-16 09:49:38 +0300

    Time: 100 ms (90.68%), Space: 17.5 MB (19.44%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,26 +1,26 @@
 class Solution:
-    def findOrder(self, c: int, pre: List[List[int]]) -> List[int]:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        glb_flag = set()
         graph = defaultdict(list)
-        for i,j in pre:
-            graph[i].append(j)
-        vstd = set()
         ans = []
-        def dfs(node):
-            if node in cycle:
-                return False
-            if node in vstd:
+        for dep,ind in prerequisites:
+            graph[ind].append(dep)
+         
+        def top_sort(node):
+            if node in glb_flag:
                 return True
-            cycle.add(node)
-            for i in graph[node]:
-                if not dfs(i):
+            if node in lcl_flag:
+                return False
+            lcl_flag.add(node)
+            for adj in graph[node]:
+                if not top_sort(adj):
                     return False
-            cycle.remove(node)
-            vstd.add(node)
+            lcl_flag.remove(node)
+            glb_flag.add(node)
             ans.append(node)
             return True
-        for i in range(c):
-            cycle = set()
-            if not dfs(i):
+        for i in range(numCourses):
+            lcl_flag = set()
+            if not top_sort(i):
                 return []
-        return ans
-        
+        return ans[::-1]
