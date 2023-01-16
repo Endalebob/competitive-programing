3060da14d43b052b51022df30c732caefc54d833
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-16 11:28:38 +0300

    Time: 658 ms (26.40%), Space: 22.8 MB (10.24%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,48 +1,42 @@
-
-
-    
-class Solution:
+class Trie:
     def __init__(self):
         self.letters = {}
-        self.is_end = False
-
-
+        self.end = False
+    
+    def insert(self,word):
+        current = self
+        for i in word:
+            if i not in current.letters:
+                current.letters[i] = Trie()
+            current = current.letters[i]
+        current.end = True
+    
+    def suggest(self,word):
+        current = self
+        for i in word:
+            if i not in current.letters:
+                return []
+            current = current.letters[i]
+        words = []
+        def find(current,word):
+            if len(words) >= 3:
+                    return
+            if current.end:
+                words.append(word)
+            letter = sorted(current.letters.keys())
+            for i in letter:
+                find(current.letters[i],word+i)
+        find(current,word)
+        return words[:3]
+class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        def insert(word):
-            current = self
-            for letter in word:
-                if letter not in current.letters:
-                    current.letters[letter] = Solution()
-                current = current.letters[letter]
-            current.is_end = True
-
-        def auto_complete(pref):
-            current = self
-            for letter in pref:
-
-                if letter not in current.letters:
-                    return []
-                current = current.letters[letter]
-
-            words = []
-
-            def dfs(current, pref):
-                if current.is_end:
-                    words.append(pref)
-                letters = sorted(current.letters.keys())
-                for letter in letters:
-                    if len(words) >= 3:
-                        return
-                    dfs(current.letters[letter], pref + letter)
-
-            dfs(current, pref)
-            return words
-        for product in products:
-            insert(product)
-        temp = ''
+        trie = Trie()
+        for i in products:
+            trie.insert(i)
+        k = ''
         ans = []
         for i in searchWord:
-            temp += i
-            ans.append(auto_complete(temp))
+            k += i
+            ans.append(trie.suggest(k))
         return ans
         
