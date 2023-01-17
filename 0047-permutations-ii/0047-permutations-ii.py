c0b08535f181ac80ec41f992ec88d81b4101ac8b
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-17 07:41:23 +0300

    Time: 78 ms (64.59%), Space: 14.2 MB (77.59%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,18 +1,16 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        def perm(nums):
-            if len(nums) == 1:
-                return [[nums[0]]]
-            temp = []
+        def ans(num,temp,ret):
+            if not num:
+                ret.append(deepcopy(temp))
             vstd = set()
-            for i in range(len(nums)):
-                if nums[i] not in vstd:
-                    vstd.add(nums[i])
-                    current = nums[i]
-                    new = nums[:i] + nums[i+1:]
-                    pem = perm(new)
-                    for i in pem:
-                        i.append(current)
-                        temp.append(i)
-            return temp
-        return perm(nums)
+            for i in range(len(num)):
+                new = num[:i]+num[i+1:]
+                if num[i] in vstd:
+                    continue
+                vstd.add(num[i])
+                temp.append(num[i])
+                ans(new,temp,ret)
+                temp.pop()
+            return ret
+        return ans(nums,[],[])
