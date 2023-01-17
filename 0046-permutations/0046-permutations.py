db71bb3ad8ef8c7b1b6337b5f7bc426404a90831
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-17 07:33:26 +0300

    Time: 54 ms (46.03%), Space: 13.8 MB (97.22%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,14 +1,13 @@
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def recursive(nums, perm=[], res=[]):
-            if not nums: # -- NOTE [1] 
-                res.append(perm[::])
-
-            for i in range(len(nums)): # [1,2,3]
-                newNums = nums[:i] + nums[i+1:]
-                perm.append(nums[i])
-                recursive(newNums, perm, res)
-                perm.pop()
-            return res
-
-        return recursive(nums)
+        def ans(num,temp,ret):
+            if not num:
+                ret.append(deepcopy(temp))
+            for i in range(len(num)):
+                new = num[:i]+num[i+1:]
+                temp.append(num[i])
+                ans(new,temp,ret)
+                temp.pop()
+            return ret
+        return ans(nums,[],[])
