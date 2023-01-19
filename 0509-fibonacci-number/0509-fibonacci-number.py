eeafb9e5591da8cac9e35a54258fd0c165a293c3
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-19 19:22:27 +0300

    Time: 34 ms (76.39%), Space: 13.9 MB (49.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        a,b = 0,1
-        ans = n
-        for i in range(2,n+1):
-            ans = a+b
-            a,b = b,ans
-        return ans
+        A=(1+5**0.5)/2
+        B=(1-5**0.5)/2
+        fi = ((pow(A,n) - pow(B,n))) // sqrt(5)
+        return int(fi)
