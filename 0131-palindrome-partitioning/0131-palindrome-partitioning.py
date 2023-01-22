f51b9994d90aa13ec18cf258946b50555d226a0e
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-01-22 14:48:32 +0300

    Time: 788 ms (44.41%), Space: 30.3 MB (62.54%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def ispalindrome(ss):
+            for i in range(len(ss)//2):
+                if ss[i] != ss[-i-1]:
+                    return False
+            return True
+        ans = []
+        def dfs(temp,idx):
+            if idx == len(s):
+                if temp:
+                    for i in temp:
+                        if not ispalindrome(i):
+                            return
+                    ans.append(temp.copy())
+                return
+            temp.append(s[idx])
+            dfs(temp,idx+1)
+            temp.pop()
+            if temp:
+                temp[-1] += s[idx]
+                dfs(temp,idx+1)
+            return ans
+        return dfs([],0)
+                    
+                    
+            
