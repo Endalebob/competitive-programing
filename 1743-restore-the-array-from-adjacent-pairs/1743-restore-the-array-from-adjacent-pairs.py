a9495583ec1f1485283f77029ef4812aebdb7205
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-02-08 21:03:42 +0300

    Time: 1141 ms (79.94%), Space: 62.9 MB (65.90%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def restoreArray(self, adjacentPairs):
+        from collections import defaultdict
+        adj_list = defaultdict(list)
+        for u, v in adjacentPairs:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        ans = [next(k for k, v in adj_list.items() if len(v) == 1)]
+        stack, visited = [adj_list[ans[0]]], set(ans)
+        while stack:
+            neighbors = stack.pop()
+            for v in neighbors:
+                if v in visited:
+                    continue
+                ans.append(v)
+                stack.append(adj_list[v])
+                visited.add(v)
+        return ans
