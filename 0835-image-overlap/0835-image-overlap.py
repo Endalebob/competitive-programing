701385526afb32e2ab25996d5e9ff96a09e58ebd
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-02-10 23:38:06 +0300

    Time: 354 ms (80.05%), Space: 14.7 MB (35.50%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -1,6 +1,6 @@
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        img1ValidIndex = [(i, j) for i, row in enumerate(img1) for j, num in enumerate(row) if num]
-        img2ValidIndex = [(i, j) for i, row in enumerate(img2) for j, num in enumerate(row) if num]
-        indexCounter = collections.Counter((i - x, j - y) for i, j in img1ValidIndex for x, y in img2ValidIndex)
+        valid_index1 = [(i, j) for i, row in enumerate(img1) for j, num in enumerate(row) if num]
+        valid_index2 = [(i, j) for i, row in enumerate(img2) for j, num in enumerate(row) if num]
+        indexCounter = collections.Counter((i - x, j - y) for i, j in valid_index1 for x, y in valid_index2)
         return 0 if not indexCounter else max(indexCounter.values())
