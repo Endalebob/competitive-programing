c04b118fa11a323ddb138b533b734affb7ebfe36
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-02-16 20:23:03 +0300

    Time: 2234 ms (51.83%), Space: 371.4 MB (16.75%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
         @lru_cache(None)
-        def get_optimal_compression_length(idx, prev_char, length, k):
+        def dp(idx, prev_char, length, k):
             if k < 0:
                 return float('inf')
             if idx >= len(s):
                 return 0
-            delete = get_optimal_compression_length(idx+1, prev_char, length, k-1)
+            delete = dp(idx+1, prev_char, length, k-1)
             keep = 0
             if s[idx] == prev_char:
                 if length in [1, 9, 99]:
                     keep = 1
-                keep += get_optimal_compression_length(idx+1, prev_char, length+1, k)
+                keep += dp(idx+1, prev_char, length+1, k)
             else:
-                keep = 1 + get_optimal_compression_length(idx+1, s[idx], 1, k)
+                keep = 1 + dp(idx+1, s[idx], 1, k)
             return min(delete, keep)
 
-        return get_optimal_compression_length(0, '', 0, k)
+        return dp(0, '', 0, k)
