51580d9464130f1f26284a7f5992f984b01ce395
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-02-26 14:16:45 +0300

    Time: 721 ms (88.89%), Space: 28.7 MB (11.11%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
@@ -1,16 +1,39 @@
 class Solution:
     def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
+        # Sort the input list in ascending order
         nums.sort()
-        hi = (len(nums)-1)//2 + 1
-        r = len(nums)-1
+        
+        # Set the upper bound of the search range to the index of the middle element
+        hi = (len(nums) - 1) // 2 + 1
+        
+        # Set the initial index for searching to the last element of the list
+        r = len(nums) - 1
+        
+        # Initialize a variable to keep track of the number of marked indices
         ans = 0
-        while hi > 0 and r >= (len(nums)-1)//2+1:
-            num = nums[r]//2
-            idx = bisect_right(nums,num,lo=0,hi=hi)
+        
+        # Keep searching for marked indices while there is still a valid search space
+        while hi > 0 and r >= (len(nums) - 1) // 2 + 1:
+            # Calculate the threshold value for the current search iteration
+            num = nums[r] // 2
+            
+            # Find the index of the first element greater than the threshold
+            # in the search range
+            idx = bisect_right(nums, num, lo=0, hi=hi)
+            
+            # If such an element exists, mark it and the middle element
             if idx > 0:
                 ans += 2
-                hi = idx-1
+                
+                # Update the upper bound of the search range to the left of the marked elements
+                hi = idx - 1
+            
+            # If no such element exists, stop searching
             else:
                 break
+            
+            # Move to the next element to the left for the next iteration
             r -= 1
+        
+        # Return the maximum number of marked indices found
         return ans
