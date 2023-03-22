b66cd0c87c58f12de1f2d6b202b431d808a24fe1
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-03-22 14:25:17 +0300

    Time: 5831 ms (5.01%), Space: 14 MB (28.18%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,5 +1,26 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        val = [str(i) for i in range(1,n+1)]
-        return ''.join(list(permutations(val,n))[k-1])
+        nums = [str(i) for i in range(1,n+1)]
+        k -= 1
+        def perm():
+            idx1 = 0
+            n = len(nums)
+            for i in range(n-2,-1,-1):
+                if nums[i] < nums[i+1]:
+                    idx1 = i
+                    break
+                    
+            idx2 = 0
+            for i in range(n-1,0,-1):
+                if nums[i] > nums[idx1]:
+                    idx2 = i
+                    break
+            nums[idx1],nums[idx2] = nums[idx2],nums[idx1]
+            nums[idx1+1:] = nums[idx1+1:][::-1]
+                
+        
+        while k:
+            perm()
+            k -= 1
+        return ''.join(nums)
         
