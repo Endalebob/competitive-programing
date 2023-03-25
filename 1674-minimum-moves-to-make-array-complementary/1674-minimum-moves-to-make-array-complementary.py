eca6d7f1e79c08a589fb9ef04720d82979690b57
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-03-25 05:30:20 +0300

    Time: 1862 ms (32.39%), Space: 46 MB (8.45%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
@@ -1,17 +1,16 @@
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
-        n = len(nums)
-        diff = defaultdict(int)
-        add = defaultdict(int)
-        for i in range(n//2):
-            a = [nums[i],nums[-1-i]]
-            add[sum(a)] += 1
-            mi,ma = min(a),max(a)
-            diff[mi+1] -= 1
-            diff[ma+limit+1] += 1
-        ans = float('inf')
-        curr = n
-        for i in range(2,2*limit+1):
-            curr += diff[i]
-            ans = min(curr-add[i],ans)
+        ans = len(nums)
+        val = defaultdict(int)
+        maxValue = limit+1
+        for i in range(len(nums)//2):
+            val[nums[i]+nums[-i-1]] += 1
+            val[nums[i]+nums[-i-1]+1] -= 1
+            val[min(nums[i],nums[-i-1])+1] += 1
+            val[max(nums[i],nums[-i-1])+limit+1] -= 1
+            maxValue = max(maxValue,max(nums[i],nums[-i-1])+limit+1)
+        curr = ans
+        for i in range(1,maxValue):
+            curr -= val[i]
+            ans = min(ans,curr)
         return ans
