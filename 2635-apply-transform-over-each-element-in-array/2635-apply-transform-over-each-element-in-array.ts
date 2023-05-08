e37e5a1b0665f9d1db45b65c5480a6760cde7737
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-05-08 09:50:17 +0300

    Time: 56 ms (83.97%), Space: 43.5 MB (31.63%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -1,7 +1,6 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    const ans:number[] = []
     for(let i=0; i<arr.length; i++){
-        ans.push((fn(arr[i],i)))
+        arr[i] = fn(arr[i],i)
     }
-    return ans
+    return arr
 };
