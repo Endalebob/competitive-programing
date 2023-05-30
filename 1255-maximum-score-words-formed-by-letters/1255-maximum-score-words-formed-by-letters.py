b8206bd64d9b99a272d378d076aa4ad4ad975801
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-05-30 14:54:06 +0300

    Time: 72 ms (41.36%), Space: 16.7 MB (9.09%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(26):
             val[chr(ord('a')+i)] = score[i]
         @lru_cache(None)
-        def dp(idx,selected):
+        def dp(idx, selected):
             if idx >= len(words):
                 return 0
             nCount = Counter(words[idx])
@@ -17,15 +17,13 @@ class Solution:
             ans = dp(idx+1,selected)
             if flag:
                 add = 0
-                m = list(selected)
-                m.append(idx)
                 
                 for i in nCount:
                     add += val[i]*nCount[i]
                     count[i] -= nCount[i]
-                ans = max(ans,dp(idx+1,tuple(m))+add)
+                ans = max(ans,dp(idx+1,selected ^ 1<<idx)+add)
                 for i in nCount:
                     count[i] += nCount[i]
             return ans
-        return dp(0,())
+        return dp(0,0)
         
