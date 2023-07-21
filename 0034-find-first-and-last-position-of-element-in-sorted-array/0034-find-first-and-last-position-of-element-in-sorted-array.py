499c3a9b109a6d91007ccc7777b7efd0c5fa2981
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-07-21 15:20:55 +0300

    Time: 84 ms (98.69%), Space: 17.8 MB (73.03%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,29 +1,29 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1,-1]
-        l,r = 0,len(nums)-1
         ans = [-1,-1]
-        while l<r:
-            m = l+(r-l)//2
-            if nums[m] < target:
-                l = m+1
-            else:
-                r = m
-        if nums[l] == target:
-            ans[0] = l
-        
-        l,r = 0,len(nums)-1
-        while l<r:
-            m = l+(r-l)//2
-            if nums[m] <= target:
-                l = m+1
-            else:
-                r = m
-        
-        if nums[l] == target:
-            ans[1] = l
-        elif nums[l-1] == target:
+        if not nums:
+            return ans
+        def first():
+            l,r = 0,len(nums)-1
+            while r>l:
+                m = l+(r-l)//2
+                if nums[m]<target:
+                    l = m+1
+                else:
+                    r = m
+            if nums[l] == target:
+                ans[0] = l
+        def last():
+            l,r = 0,len(nums)
+            while r>l:
+                m = l+(r-l)//2
+                if nums[m]<=target:
+                    l = m+1
+                else:
+                    r = m
+            if nums[l-1] == target:
                 ans[1] = l-1
-            
+        first()
+        last()
         return ans
+        
