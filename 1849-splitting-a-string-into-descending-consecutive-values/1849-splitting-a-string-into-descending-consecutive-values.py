94d94ac9d8d09e7c365840bbab669b72c744c3d4
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-07-26 15:32:42 +0300

    Time: 40 ms (96.81%), Space: 16.3 MB (61.43%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -1,13 +1,13 @@
 class Solution:
     def splitString(self, s: str) -> bool:
         n = len(s)
-        def rec(idx,part,prev):
+        def rec(idx,cnt,prev):
             if idx == n:
-                return part >= 2
+                return cnt >= 2
             for i in range(idx,n):
                 new = int(s[idx:i+1])
-                if prev == '+' or prev-1 == new:
-                    if rec(i+1,part+1,new):
+                if cnt == 0 or prev-1 == new:
+                    if rec(i+1,cnt+1,new):
                         return True
             return False
-        return rec(0,0,'+')
+        return rec(0,0,-1)
