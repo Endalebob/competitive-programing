2bdd8c12a446f60f9b6e4ff985a8758603361f50
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-07-26 15:04:56 +0300

    Time: 435 ms (25.72%), Space: 18.2 MB (90.33%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,12 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def ans(l,temp,ret):
+        combinations = []
+        nums = [i for i in range(1,n+1)]
+        def comb(idx,temp):
             if len(temp) == k:
-                ret.append(deepcopy(temp))
-                return 
-            for i in range(l,n+1):
-                temp.append(i)
-                ans(i+1,temp,ret)
-                temp.pop()
-            return ret
-        return ans(1,[],[])
+                combinations.append(temp[:])
+                return
+            if idx>=n:
+                return
+            temp.append(nums[idx])
+            comb(idx+1,temp)
+            temp.pop()
+            comb(idx+1,temp)
+        comb(0,[])
+        return combinations
+            
