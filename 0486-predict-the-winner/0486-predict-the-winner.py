d9535d253a7e2e67503184d8b42f490e5ac90ab1
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-07-28 14:43:50 +0300

    Time: 151 ms (28.03%), Space: 16.3 MB (70.15%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,7 +1,24 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool: 
-        def find(i, j):
-            if i == j:
-                return nums[i]
-            return max(nums[i]-find(i+1, j), nums[j]-find(i, j-1))
-        return find(0, len(nums)-1) >= 0
+        def f( start, end, person) -> bool :
+            if start > end :
+                return scores [ 0 ] >= scores [ 1 ]             
+            
+            scores [ person ] += nums [ start ]
+            case1  = f( start+1, end,( person+1 )%2 )
+            scores [ person ] -= nums [ start ]
+            
+            if person is 0 and case1 is True :
+                return True
+            
+            if person is 1 and case1 is False :
+                return False
+            
+            scores [ person ] += nums [ end ]
+            case2  = f( start, end-1 ,( person+1 )%2 )
+            scores [ person ] -= nums [ end ]  
+            
+            return case2
+        
+        scores = [0, 0]
+        return f(0, len(nums)-1, 0)
