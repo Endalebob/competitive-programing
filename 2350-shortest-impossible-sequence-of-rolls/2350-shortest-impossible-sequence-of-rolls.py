dd2b6932c58f22cb9838faf1d31bd16c77b82d98
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-05 11:37:12 +0300

    Time: 858 ms (49.04%), Space: 30.7 MB (25.96%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
@@ -2,14 +2,11 @@ class Solution:
     def shortestSequence(self, rolls: List[int], k: int) -> int:
         ans = 1
         unique = set()
-        deq = deque()
         for i in rolls:
             prev = len(unique)
             unique.add(i)
             if prev < len(unique):
-                deq.append(i)
-                if len(deq) == k:
+                if len(unique) == k:
                     ans += 1
                     unique = set()
-                    deq = deque()
         return ans
