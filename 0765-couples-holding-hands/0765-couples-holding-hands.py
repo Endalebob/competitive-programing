d4211cdb70b4a87df622990c4185893598cdeb0f
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-07 10:38:25 +0300

    Time: 39 ms (80.26%), Space: 16.4 MB (45.61%) - LeetHub

--- 0765-couples-holding-hands/0765-couples-holding-hands.py
+++ 0765-couples-holding-hands/0765-couples-holding-hands.py
@@ -1,43 +1,15 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parents = [i for i in range(n)]
-        self.rank = [1 for i in range(n)]
-        self.num_dis_set = n
-
-    def find(self, xp):
-        compress = []
-        while xp != self.parents[xp]:
-            compress.append(xp)
-            xp = self.parents[xp]
-
-        for i in compress:
-            self.parents[i] = xp
-
-        return xp
-
-    def union(self, a, b):
-        ap, bp = self.find(a), self.find(b)
-
-        if ap == bp:
-            return 0
-
-        if self.rank[ap] < self.rank[bp]:
-            ap, bp = bp, ap
-        self.parents[bp] = ap
-        self.rank[ap] += self.rank[bp]
-
-        return 1
-
-    def size(self, x):
-        return self.rank[self.find(x)]
-
 class Solution:
     def minSwapsCouples(self, row: List[int]) -> int:
-        uf = UnionFind(len(row))
-        for i in range(0,len(row),2):
-            uf.union(row[i],row[i+1])
         ans = 0
-        for i in range(0,len(row),2):
-            ans += uf.union(i,i+1)
+        for i in range(1,len(row),2):
+            if row[i-1] > row[i]:
+                row[i-1],row[i] = row[i],row[i-1]
+            if abs(row[i-1]-row[i]) > 1 or row[i] % 2 == 0:
+                if row[i-1] % 2:
+                    idx = row.index(row[i-1]-1)
+                else:
+                    idx = row.index(row[i-1]+1)
+                row[idx],row[i] = row[i],row[idx]
+                ans += 1
         return ans
-        
+            
