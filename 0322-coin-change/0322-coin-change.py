c8158bfa7dc965e7522e9421156398bec42cab01
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-27 21:51:46 +0300

    Time: 2325 ms (5.00%), Space: 471 MB (5.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        '''
-        our goal is to make the total amount of money using a few coin.
-        there may be many posibilities of getting that amount of money
-        we check every possibilities and we take the one with few coin.
-        
-        while we are perfoming combination there may be some overlap
-        it isn't necessary to solve again again the same problem in order
-        not to solve the same problem again again we need to memorize which 
-        problem we already solved.
-        '''
-        memo = {amount:0}
-        for tot in range(amount-1,-1,-1):
-            cur = float('inf')
-            for i in coins:
-                if tot + i in memo:
-                    cur = min(cur,memo[tot+i]+ 1)
-            memo[tot] = cur
-        if memo[0] == float('inf'):
-            return -1
-        return memo[0]
-        
+        memo = {}
+        def dp(idx,curr):
+            if curr == amount:
+                return 0
+            if curr > amount or idx>=len(coins):
+                return inf
+            if (idx,curr) not in memo:
+                memo[idx,curr] = min(dp(idx+1,curr),dp(idx,curr+coins[idx])+1)
+            return memo[idx,curr]
+        ans = dp(0,0)
+        if ans != inf:
+            return ans
+        return -1
