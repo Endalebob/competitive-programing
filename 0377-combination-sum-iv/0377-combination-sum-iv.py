96fb81e3165c9227a53c9ec7e9e1e7faf84920d1
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-28 14:35:08 +0300

    Time: 56 ms (8.03%), Space: 16.6 MB (14.10%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -2,10 +2,10 @@ class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         @lru_cache(None)
         def dp(curr):
-            if curr > target:
-                return 0
             if curr == target:
                 return 1
+            if curr > target:
+                return 0
             ans = 0
             for i in nums:
                 ans += dp(curr+i)
