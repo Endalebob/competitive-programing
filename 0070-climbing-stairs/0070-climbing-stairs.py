0523e7a8c4035ab3f8085bc86387577c22d606ce
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-28 14:17:45 +0300

    Time: 44 ms (14.85%), Space: 16.2 MB (57.40%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,9 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         @lru_cache(None)
         def dp(idx):
-            if idx > n:
-                return 0
             if idx == n:
                 return 1
+            if idx > n:
+                return 0
             return dp(idx+1) + dp(idx+2)
         return dp(0)
