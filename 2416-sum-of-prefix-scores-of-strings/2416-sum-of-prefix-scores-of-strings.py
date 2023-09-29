3e53654f179b88c9aca31bc13f30336da0335901
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-29 15:35:08 +0300

    Time: 2771 ms (75.09%), Space: 415.2 MB (80.38%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -1,19 +1,15 @@
-
 class Trie:
-
     def __init__(self):
         self.letters = {}
         self.count = 0
-
-    def insert(self, word: str) -> None:
+    def insert(self,word):
         current = self
         for i in word:
             if i not in current.letters:
                 current.letters[i] = Trie()
             current = current.letters[i]
             current.count += 1
-
-    def search(self, word: str) -> bool:
+    def find(self,word):
         current = self
         ans = 0
         for i in word:
@@ -22,6 +18,11 @@ class Trie:
         return ans
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
-        dic = Trie()
-        for i in words: dic.insert(i)
-        return [dic.search(i) for i in words]
+        trie = Trie()
+        for i in words:
+            trie.insert(i)
+        ans = []
+        for i in words:
+            ans.append(trie.find(i))
+        return ans
+        
