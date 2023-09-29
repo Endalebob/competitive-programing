cdb2bb106cb2fe752143068839df904917bd6aca
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-09-29 17:44:37 +0300

    Time: 523 ms (17.70%), Space: 25.1 MB (14.82%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -2,7 +2,6 @@ class Trie:
     def __init__(self):
         self.letters = {}
         self.end = False
-    
     def insert(self,word):
         current = self
         for i in word:
@@ -10,33 +9,36 @@ class Trie:
                 current.letters[i] = Trie()
             current = current.letters[i]
         current.end = True
-    
-    def suggest(self,word):
+    def suggestion(self,pre):
         current = self
-        for i in word:
+        self.words = []
+        for i in pre:
             if i not in current.letters:
                 return []
             current = current.letters[i]
-        words = []
-        def find(current,word):
-            if len(words) >= 3:
-                    return
-            if current.end:
-                words.append(word)
-            letter = sorted(current.letters.keys())
-            for i in letter:
-                find(current.letters[i],word+i)
-        find(current,word)
-        return words[:3]
+        self.dfs(current,pre)
+        return self.words[:3]
+    def dfs(self,current,word):
+        if current.end:
+            self.words.append(word)
+        if len(self.words) >= 3:
+                return
+        letter = sorted(current.letters.keys())
+        for i in letter:
+            self.dfs(current.letters[i],word+i)
+  
+        
+        
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         trie = Trie()
+        curr = ''
         for i in products:
             trie.insert(i)
-        k = ''
         ans = []
         for i in searchWord:
-            k += i
-            ans.append(trie.suggest(k))
+            curr += i
+            ans.append(trie.suggestion(curr))
         return ans
+            
         
