0ea3ca4ed01c5d0d1e2ec8c6b3eb25d2932dd660
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-10-12 15:38:33 +0300

    Time: 36 ms (72.40%), Space: 17.1 MB (60.00%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -8,40 +8,36 @@
 
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        #the first step is to find the mountain
-        l,r = 0,mountain_arr.length()-1
-        if r < 2:
-            return -1
-        while l < r:
-            m = l + (r-l)//2
-            if mountain_arr.get(m) < mountain_arr.get(m+1):
-                l = m+1
-            else:
-                r = m
+        right = mountain_arr.length()-1
+        def find_pick():
+            l,r = 0, right
+            while l<r:
+                mid = l + (r-l)//2
+                if mountain_arr.get(mid-1) < mountain_arr.get(mid) < mountain_arr.get(mid+1):
+                    l = mid
+                elif mountain_arr.get(mid-1) > mountain_arr.get(mid) > mountain_arr.get(mid+1):
+                    r = mid
+                else:
+                    return mid
+        pick = find_pick()
         
-        #check in the first part
-        val = l
-        r = val
-        l = 0
-        while l <= r:
-            m = l + (r-l)//2
-            if mountain_arr.get(m) == target:
-                return m
-            if mountain_arr.get(m) > target:
-                r = m -1
-            else:
-                l = m + 1
-        
-        #second check
-        
-        l = val+1
-        r = mountain_arr.length()-1
-        while l <= r:
-            m = l + (r-l)//2
-            if mountain_arr.get(m) == target:
-                return m
-            if mountain_arr.get(m) < target:
-                r = m -1
-            else:
-                l = m + 1 
-        return -1
+        def find_left_most(l,r):
+            while l<r:
+                mid = l + (r-l)//2
+                if mountain_arr.get(mid) >= target:
+                    r = mid
+                else:
+                    l = mid+1
+            return l if mountain_arr.get(l) == target else -1
+        def find_leftt_most(l,r):
+            while l<r:
+                mid = l + (r-l)//2
+                if mountain_arr.get(mid) <= target:
+                    r = mid
+                else:
+                    l = mid+1
+            return l if mountain_arr.get(l) == target else -1
+        ans = find_left_most(0,pick)
+        if ans != -1:
+            return ans
+        return find_leftt_most(pick,right)
