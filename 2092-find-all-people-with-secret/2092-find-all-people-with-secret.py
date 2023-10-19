7dbb1288cc799bacc1cfd1524d3e6f509c21d391
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-10-20 00:12:12 +0300

    Time: 2026 ms (35.20%), Space: 53.8 MB (89.60%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,24 +1,40 @@
-class Solution:
+class UnionFind:
+    def __init__(self,n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1]*n
     def find(self,a):
-        if a != self.root[a]:
-            self.root[a] = self.find(self.root[a])
-        return self.root[a]
+        compress = []
+        while a != self.parent[a]:
+            compress.append(a)
+            a = self.parent[a]
+        for i in compress:
+            self.parent[i] = a
+        return a
+            
     def union(self,a,b):
-        self.root[self.find(b)] = self.find(a)
-    def isConnected(self,a,b):
-        return self.find(a) == self.find(b)
-    def findAllPeople(self, n: int, m: List[List[int]], f: int) -> List[int]:
-        self.root = [i for i in range(n)]
-        self.union(0,f)
-        dic = defaultdict(list)
-        for i in m:
-            dic[i[2]].append(i[:2])
-        for i in sorted(dic.keys()):
-            vstd = set()
-            for a,b in dic[i]:
-                self.union(a,b)
-                vstd.update({a,b})
-            for j in vstd:
-                if not self.isConnected(0,j):
-                    self.root[j] = j
-        return [i for i in range(n) if self.isConnected(0,i)]  
+        ap,bp = self.find(a),self.find(b)
+        if ap == bp:
+            return
+        if self.rank[ap] < self.rank[bp]:
+            ap,bp = bp,ap
+        self.parent[bp] = ap
+        self.rank[ap] += 1
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        uf = UnionFind(n)
+        uf.union(0,firstPerson)
+        
+        time = defaultdict(list)
+        for a,b,t in meetings:
+            time[t].append((a,b))
+        keys = sorted(time.keys())
+        z_p = uf.find(0)
+        for i in keys:
+            for j,k in time[i]:
+                uf.union(j,k)
+            for j,k in time[i]:
+                if uf.find(j) != uf.find(0) or uf.find(k) != uf.find(0):
+                    uf.parent[j] = j
+                    uf.parent[k] = k
+        return [i for i in range(n) if uf.find(i) == uf.find(0)]
+        
