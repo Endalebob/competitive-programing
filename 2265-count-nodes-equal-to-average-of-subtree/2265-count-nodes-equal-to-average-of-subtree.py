a0ab7a40364fa1ee2d71f06bc021cc525984e490
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-11-03 00:08:38 +0300

    Time: 48 ms (63.30%), Space: 17.6 MB (57.44%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -6,15 +6,13 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
-        def dfs(root):
-            if not root:
-                return 0,0,0
-            su_ml,numl,ansl = dfs(root.left)
-            su_mr,numr,ansr = dfs(root.right)
-            ans = ansl+ansr
-            total = su_ml+su_mr + root.val
-            total_num = numl+numr+1
-            if root.val == total//total_num:
-                ans += 1
-            return total,total_num,ans
-        return dfs(root)[2]
+        self.ans = 0
+        def dfs(node):
+            if not node:
+                return 0,0
+            left_cnt, left_sum = dfs(node.left)
+            right_cnt, right_sum = dfs(node.right)
+            self.ans += ((left_sum+right_sum+node.val)//(left_cnt+right_cnt+1)) == node.val
+            return left_cnt+right_cnt+1,left_sum+right_sum+node.val
+        dfs(root)
+        return self.ans
