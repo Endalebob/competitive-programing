97a4f3269df7b8997c878df7634ae29c6c522f31
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-11-12 20:40:55 +0300

    Time: 3447 ms (7.33%), Space: 453.9 MB (5.29%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,24 +1,30 @@
 class Solution:
-    def numBusesToDestination(self, rts: List[List[int]], src: int, tgt: int) -> int:
-        g = defaultdict(list)
-        for i, r in enumerate(rts):
-            p = r[0]
-            for j in range(1, len(r)):
-                g[p].append((r[j], i))
-                g[r[j]].append((p, i))
-                p = r[j]
-
-        h = [(0, 0, src, None)]
-        v = set()
-        c = 1
-        while len(h) > 0:
-            bc, o, cur, prev = heapq.heappop(h)
-            if cur == tgt:
-                return bc
-            v.add((cur, prev))
-            for nxt, cb in g[cur]:
-                nbc = bc + (1 if cb != prev else 0)
-                if (nxt, cb) not in v:
-                    heapq.heappush(h, (nbc, c, nxt, cb))
-                    c += 1
-        return -1       
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        relation = defaultdict(list)
+        graph = defaultdict(list)
+        for i in range(len(routes)):
+            for j in routes[i]:
+                if relation[j]:
+                    for k in relation[j]:
+                        graph[k].append(i)
+                        graph[i].append(k)
+                relation[j].append(i)
+            routes[i] = set(routes[i])
+        deq = []
+        vstd = set()
+        for i in relation[source]:
+            deq.append((i,1))
+            vstd.add(i)
+        deq = deque(deq)
+        while deq:
+            if source == target:
+                return 0
+            node,cost = deq.popleft()
+            if target in routes[node]:
+                return cost
+            for i in graph[node]:
+                if i not in vstd:
+                    vstd.add(i)
+                    deq.append((i,cost+1))
+        return -1
+        
