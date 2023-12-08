8a80e785f6a529c80497075b8f101f76cb998624
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-12-08 10:58:37 +0300

    Time: 70 ms (39.16%), Space: 28.6 MB (49.42%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,17 +6,27 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(self, root: 'TreeNode', a: 'TreeNode', b: 'TreeNode') -> 'TreeNode':
         self.ans = None
-        def dfs(root):
-            if not root:
+        
+        def dfs(node):
+            if not node:
                 return False
-            left = dfs(root.left)
-            right = dfs(root.right)
-            mid = (root == p) or (root == q)
-            tot = left + right + mid
-            if tot>1:
-                self.ans = root
-            return tot>0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            # Check if the current node matches one of the target nodes
+            mid = (node == a) or (node == b)
+            
+            # Calculate the total nodes found in left, right, and current node
+            total_nodes = left + right + mid
+            
+            if total_nodes > 1:
+                self.ans = node  # Update the lowest common ancestor
+            
+            # Return True if at least one target node is found in the subtree
+            return total_nodes > 0
+        
         dfs(root)
         return self.ans
